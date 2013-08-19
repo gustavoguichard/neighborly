3912aef8a9f32e54d05495c243ca120a0910a7f0
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-19 17:28:13 -0500

    Fix project commets javascript specs

--- app/assets/javascripts/app/project/project_comments.js
+++ app/assets/javascripts/app/project/project_comments.js
@@ -2,14 +2,11 @@ App.views.Project.addChild('ProjectComments', {
   el: '#project_comments',
 
   activate: function(){
-    this.render();
+    window.setTimeout(this.replaceCommentsCount, 4000);
   },
 
-  render: function(){
-    replaceCommentsCount = function() {
-      $('span#countNumber').text($('a#disqusCount').text().split(" ")[0]);
-      $('.commentsCount').show();
-    }
-    setTimeout('replaceCommentsCount()', 4000);
+  replaceCommentsCount: function(){
+    $('span#countNumber').text($('a#disqusCount').text().split(" ")[0]);
+    $('.commentsCount').show();
   }
 });
