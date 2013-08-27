d9e7708f6ddaf8192e128cc96438baf0fefb0937
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-27 13:28:23 -0500

    Change user credentials verification (omniauth) for existing google accounts

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,12 +3,32 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     OauthProvider.all.each do |p|
       define_method p.name.downcase do
         omniauth = request.env['omniauth.auth']
-        @user = User.
-          select('users.*').
-          joins('JOIN authorizations ON authorizations.user_id = users.id').
-          joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
-          where("authorizations.uid = :uid AND oauth_providers.name = :provider", {uid: omniauth[:uid], provider: p.name}).
-          first || User.create_with_omniauth(omniauth, current_user)
+
+        # The google oauth 2 is different because the migration of old
+        # database, since we don't had the uid...
+        # We can remove this when all users be with the uid saved.
+        if p.name == 'google_oauth2'
+            select('users.*').
+            joins('JOIN authorizations ON authorizations.user_id = users.id').
+            joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
+            where("users.email = :email AND oauth_providers.name = :provider", {email: omniauth['info']['email'], provider: p.name}).
+            first || User.create_with_omniauth(omniauth, current_user)
+
+            # Save the uid for old users that don't have it.
+            # We can remove this when all users be with the uid saved.
+            authorization = @user.authorizations.where(provider: 'google_oauth2').first
+            if authorization and not authorization.uid.present?
+              authorization.uid = omniauth[:uid]
+              authorization.save
+            end
+        else
+          @user = User.
+            select('users.*').
+            joins('JOIN authorizations ON authorizations.user_id = users.id').
+            joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
+            where("authorizations.uid = :uid AND oauth_providers.name = :provider", {uid: omniauth[:uid], provider: p.name}).
+            first || User.create_with_omniauth(omniauth, current_user)
+        end
 
         flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: p.name.capitalize)
         sign_in @user, event: :authentication
