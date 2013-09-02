bf2eaf5b524b48d31b35ba4a2f92d31a2bfbe164
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-02 10:01:55 -0500

    Adjust states array to show the acronym

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,13 +1,14 @@
 class State < ActiveRecord::Base
   validates_presence_of :name, :acronym
-  validates_uniqueness_of :name, :acronym
+  validates_uniqueness_of :name, scope: :acronym
+  validates_uniqueness_of :acronym, scope: :name
+
   def self.array
     return @array if @array
     @array = []
     self.order(:name).each do |state|
-      @array << [state.name, state.acronym]
+      @array << ["#{state.name} - #{state.acronym}", state.acronym]
     end
-    @array.push(['Outro / Other', 'outro / other'])
     @array
   end
 end
