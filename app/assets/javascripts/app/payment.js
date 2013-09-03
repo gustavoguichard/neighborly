bae6eeffc6a7aeb9d17b50c83d3eb5f3f2804047
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-03 13:25:58 -0500

    Migrate checkout page design

--- app/assets/javascripts/app/payment.js
+++ app/assets/javascripts/app/payment.js
@@ -2,12 +2,25 @@ App.addChild('Payment', _.extend({
   el: '#main_content[data-action="create"][data-controller-name="backers"] #payment',
 
   events: {
-    'click #payment_menu a' : 'onTabClick'
+    'change .payment_menu input' : 'showPaymentContent'
+  },
+
+  showPaymentContent: function(e){
+    $('#info_box strong.total_value').text($(e.target).data('value-with-taxs'));
+    $('.payments_type').hide();
+    payment = '.payments_type#' + $(e.target).val() + '_payment'
+    $(payment).fadeIn();
+
+    if($.trim($(payment).html()) == '' && $(payment).data('path')){
+      $.get($(payment).data('path')).success(function(data){
+        $(payment).html(data);
+      });
+    }
   },
 
   activate: function(){
-    this.onTabClick({target: this.$('#payment_menu a:first')});
+    this.$('.payment_menu input:first').attr('checked', true);
+    this.showPaymentContent({target: '#payment_method_credit_card_net'});
   }
-
-}, Skull.Tabs));
+}));
 
