6e0b35bc0fabcfc5697d62827592f9fae8d9c8e3
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-04 16:07:41 -0500

    Changes on projects/new

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -70,7 +70,7 @@ describe "Projects" do
       [
         'permalink', 'name', 'video_url',
         'headline', 'goal', 'online_days',
-        'about', 'first_backers', 'how_know', 'more_links'
+        'about', 'first_backers', 'how_know'
       ].each do |a|
         fill_in "project_#{a}", with: project.attributes[a]
       end
