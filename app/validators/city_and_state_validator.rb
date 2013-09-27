ae31766515ac411eeeb5963f898f7cf4736eefee
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-27 10:10:10 -0500

    Solve problem with city and state validation

--- app/validators/city_and_state_validator.rb
+++ app/validators/city_and_state_validator.rb
@@ -4,10 +4,8 @@ class CityAndStateValidator < ActiveModel::Validator
       if record.address_city.present? && record.address_state.present?
         result = Geocoder.search(record.address, params: { countrycodes: "us" })
 
-        if result.first.present?
-          record.address_city = result.first.city
-          record.address_state = result.first.state_code
-        end
+        record.address_city = result.first.city rescue nil
+        record.address_state = result.first.state_code rescue nil
 
         record.errors.add(:address, I18n.t('activerecord.validators.city_and_state.invalid')) unless record.address_city.present? && record.address_state.present?
       elsif not options.include?(:allow_blank)
