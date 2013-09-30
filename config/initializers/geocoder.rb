9671688b59f829a0b6aca045dd831d1888496211
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-30 11:58:47 -0500

    Add google maps key to geocoder

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,8 @@
+Geocoder.configure(
+
+  # geocoding service (see below for supported options):
+  lookup: :google,
+
+  # to use an API key:
+  api_key: 'AIzaSyCcgfuyH0AP0i3eCH8ClxArAw2rEDjiTK8',
+)
