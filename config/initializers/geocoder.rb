5224f3a3f6546c941c7daa0f2f50c83b834c821f
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-09-30 12:31:08 -0500

    Remove key from geocoder

--- config/initializers/geocoder.rb
+++
@@ -1,8 +0,0 @@
-Geocoder.configure(
-
-  # geocoding service (see below for supported options):
-  lookup: :google,
-
-  # to use an API key:
-  api_key: 'AIzaSyCcgfuyH0AP0i3eCH8ClxArAw2rEDjiTK8',
-)
