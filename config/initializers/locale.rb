ea7759aab797238b97f4ae4ca3b3c5332f95ab70
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-10-22 17:38:57 -0500

    Disable old locales and add some missing

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,8 +4,8 @@ I18n.load_path += Dir[Rails.root.join('app', 'locales', '**', '*.{rb,yml}').to_s
 I18n.load_path += Dir[Rails.root.join('app', 'locales', '*.{rb,yml}').to_s]
 
 # Old locales location
-I18n.load_path += Dir[Rails.root.join('lib', 'locales', '*.{rb,yml}')]
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+#I18n.load_path += Dir[Rails.root.join('lib', 'locales', '*.{rb,yml}')]
+#I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # set available locales from config/locales files
 #I18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.{yml}')].collect {|l| locale = l.scan(/.+\/(..)\.yml/)[0]; locale[0].to_sym unless locale.nil?}.delete_if {|locale| locale.nil?}
