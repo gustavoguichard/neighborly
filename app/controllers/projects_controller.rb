fbe83b12c0ba8c5d35e9d41102d34acfa19f55ec
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-10-24 12:34:41 -0500

    Add suport for about the project on a xhr request

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,7 @@ class ProjectsController < ApplicationController
 
   def show
     fb_admins_add(resource.user.facebook_id) if resource.user.facebook_id
+    render :about if request.xhr?
   end
 
   def comments
