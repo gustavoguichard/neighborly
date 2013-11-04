3e8de7dea7e473bf5dc08b968c0f8c6b8620c32d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-04 14:29:59 -0600

    Adjust design for project faqs

--- app/controllers/projects/faqs_controller.rb
+++ app/controllers/projects/faqs_controller.rb
@@ -6,11 +6,19 @@ class Projects::FaqsController < ApplicationController
   actions :index, :create, :destroy
   belongs_to :project, finder: :find_by_permalink!
 
+  def index
+    @project = parent
+  end
+
   def create
     create! { project_faqs_path(parent) }
   end
 
   def destroy
     destroy! { project_faqs_path(parent) }
+  end
+
+  def collection
+    @faqs ||= end_of_association_chain.order('created_at desc')
   end
 end
