a3f65e56bf1b838704d3c0de1e77dbe442c645eb
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-05 17:59:28 -0600

    Start changes for discover page

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,12 +1,40 @@
 class DiscoverController < ApplicationController
+
+  FILTERS = [ 'recommended', 'expiring', 'recent', 'successful', 'soon' ]
+
   def index
-    @title = t('explore.title')
-    ## Just to know if we should present the menu entries, the actual projects are fetched via AJAX
-    #@recommended = Project.not_soon.visible.not_expired.recommended.limit(1)
-    #@expiring = Project.not_soon.visible.expiring.limit(1)
-    #@recent = Project.not_soon.visible.recent.not_expired.limit(1).order('created_at DESC')
-    #@successful = Project.not_soon.visible.successful.limit(1)
-    #@soon = Project.soon.visible.limit(1)
-    @categories = Category.with_projects.order(:name_pt)
+    @avaliable_filters = FILTERS
+    @filters = {}
+    @tags = Tag.all
+    @projects = Project.visible.order_for_search
+
+    if params[:filter].present? && FILTERS.include?(params[:filter].downcase)
+      @projects = @projects.send(params[:filter].downcase)
+      @filters.merge! filter: params[:filter].downcase
+    end
+
+    if params[:near].present?
+      @projects = @projects.near(params[:near], 30)
+      @filters.merge! near: params[:near]
+    end
+
+    if params[:category].present?
+      category = Category.where('name_en ILIKE ?', params[:category]).first
+      if category.present?
+        @projects = @projects.by_category_id(category.id)
+        @filters.merge! category: category.name_en
+      end
+    end
+
+    if params[:tags].present?
+      tags = Project::TagList.new params[:tags]
+      @projects = @projects.joins("JOIN taggings ON taggings.project_id = projects.id AND taggings.tag_id IN (SELECT id FROM tags WHERE tags.name IN (#{tags.map { |t| "'#{t}'"}.join(',')}))")
+      @filters.merge! tags: tags
+    end
+
+    if params[:search].present?
+      @projects = @projects.pg_search(params[:search])
+      @filters.merge! search: params[:search]
+    end
   end
 end
