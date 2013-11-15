061e42f0adc64f667fb3650474e71e94d78ed57b
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-15 10:43:30 -0600

    Rename hero image version name and blur percentage

--- app/uploaders/hero_image_uploader.rb
+++ app/uploaders/hero_image_uploader.rb
@@ -2,20 +2,20 @@
 
 class HeroImageUploader < ImageUploader
 
-  version :hero_image
+  version :blur
 
   def store_dir
     "uploads/project/#{mounted_as}/#{model.id}"
   end
 
-  version :hero_image do
+  version :blur do
     process :apply_blur
     process convert: :png
   end
 
   def apply_blur
     manipulate! do |img|
-      img = img.blur_image(0, 2.5)
+      img = img.blur_image(0, 3)
       img
     end
   end
