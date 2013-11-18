0f26dd5b71fe0f2ec4f7e7dfcd3f70341fdf7f3e
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-18 15:56:45 -0600

    Improve the feature project spec

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -90,31 +90,67 @@ describe "Projects" do
   end
 
   describe "tabs" do
-    let(:project) { create(:project, online_days: 10, state: 'online', user: current_user) }
-
     before do
       login
       visit project_path(project)
     end
 
-    it 'updates tab should be present' do
-      page.should have_selector("a[href='#{project_updates_path(project)}']")
+   context 'when is my project' do
+      let(:project) { create(:project, online_days: 10, state: 'online', user: current_user) }
+
+      describe '#updates' do
+        it 'should be able to click on updates tab' do
+           find("a[href='#{project_updates_path(project)}']").click
+           current_path.should == project_updates_path(project)
+        end
+      end
+
+      describe '#backers' do
+        it 'should be able to click on backers tab' do
+           find("a[href='#{project_backers_path(project)}']").click
+           current_path.should == project_backers_path(project)
+        end
+      end
+
+      describe '#comments' do
+        it 'should be able to click on comments tab' do
+           find("a[href='#{comments_project_path(project)}']").click
+           current_path.should == comments_project_path(project)
+        end
+      end
+
+      describe '#faqs' do
+        it 'should be able to click on faqs tab' do
+           find("a[href='#{project_faqs_path(project)}']").click
+           current_path.should == project_faqs_path(project)
+        end
+      end
+
+      describe '#terms' do
+        it 'should be able to click on terms tab' do
+           find("a[href='#{project_terms_path(project)}']").click
+           current_path.should == project_terms_path(project)
+        end
+      end
+
+      describe '#reports' do
+        it 'should be able to click on reports tab' do
+           find("a[href='#{reports_project_path(project)}']").click
+           current_path.should == reports_project_path(project)
+        end
+      end
     end
 
-    it 'backers tab should be present' do
-      page.should have_selector("a[href='#{project_backers_path(project)}']")
-    end
+    context 'when is not my project' do
+      let(:project) { create(:project, online_days: 10, state: 'online') }
 
-    it 'comments tab should be present' do
-      page.should have_selector("a[href='#{comments_project_path(project)}']")
-    end
+      it 'should not have reports tab' do
+        page.should_not have_selector("a[href='#{reports_project_path(project)}']")
+      end
 
-    it 'faqs tab should be present' do
-      page.should have_selector("a[href='#{project_faqs_path(project)}']")
-    end
-
-    it 'terms tab should be present' do
-      page.should have_selector("a[href='#{project_terms_path(project)}']")
+      it 'should not have edit link' do
+        page.should_not have_selector("a[href='#{edit_project_path(project)}']")
+      end
     end
   end
 end
