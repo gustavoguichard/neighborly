77618abf0bd414794d4f0fd5574ebe946e20bf82
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-18 17:29:32 -0600

    Move custom form helper to feature helpers

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -14,6 +14,21 @@ module FeatureHelpers
   end
 end
 
+module CustomFormsHelper
+  def foundation_select(option, opts={})
+    # Zurb Foundation adds custom markup after (and then hides)
+    # the originating select. Here we simulate the user's interaction
+    # with the custom form instead of just setting the hidden originating select's value
+    originating_select_name = opts[:from]
+
+    custom_select = find("select[name='#{originating_select_name}'] + .custom.dropdown")
+    # click dropdown trigger
+    custom_select.find('a.selector').click
+    # click option li with correct option text
+    custom_select.find('li', text: option).click
+  end
+end
+
 RSpec.configure do |config|
   config.include FeatureHelpers, type: :feature
   config.include CustomFormsHelper, type: :feature
