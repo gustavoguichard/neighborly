f2b7ea68cb0f583377a3656754156e3b3e183ea7
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-11-25 14:49:14 -0600

    Remove unused auto_html filter email_image

--- lib/catarse_auto_html.rb
+++ lib/catarse_auto_html.rb
@@ -1,11 +1,4 @@
 module CatarseAutoHtml
-  AutoHtml.add_filter(:email_image).with(width: 200) do |text, options|
-    text.gsub(/http(s)?:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
-      width = options[:width]
-      %|<img src="#{match}" alt="" style="max-width:#{width}px" />|
-    end
-  end
-
   def catarse_auto_html_for options={}
     self.auto_html_for options[:field] do
       unless options[:not_escape_html]
