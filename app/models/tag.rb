038b5ddae145a288fe8587f1f50a1d63b415786e
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-04 10:34:34 -0600

    Add order by name on popular tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,7 +5,7 @@ class Tag < ActiveRecord::Base
   before_save :lowercase_name
 
   scope :popular, -> do
-    where("exists(select true from taggings where tag_id = tags.id and taggings.tag_id in (select t.tag_id from taggings t where t.project_id in (select p.id from projects p where p.state not in('draft', 'rejected', 'in_analysis')) group by t.tag_id having count(*) > 3))")
+    where("exists(select true from taggings where tag_id = tags.id and taggings.tag_id in (select t.tag_id from taggings t where t.project_id in (select p.id from projects p where p.state not in('draft', 'rejected', 'in_analysis')) group by t.tag_id having count(*) > 3))").order(:name)
   end
 
   def decorator
