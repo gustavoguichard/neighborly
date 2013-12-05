64f841ff3cd346840cd366da3c1bb6050894b1b2
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-05 13:20:07 -0600

    Update default user avatar image

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -18,7 +18,7 @@ class UserDecorator < Draper::Decorator
 
   def gravatar_url
     return unless source.email
-    "https://gravatar.com/avatar/#{Digest::MD5.new.update(source.email)}.jpg?default=#{::Configuration[:base_url]}/assets/user.png"
+    "https://gravatar.com/avatar/#{Digest::MD5.new.update(source.email)}.jpg?default=#{::Configuration[:base_url]}/assets/default-avatar.png"
   end
 
   def display_name
