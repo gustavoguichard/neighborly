95648a81f877497751b4c0b62a99096ffeb75dcf
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-18 15:15:45 -0600

    Create new flow to sign in/up using social networks

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,56 +1,37 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def self.add_providers
     OauthProvider.all.each do |p|
       define_method p.name.downcase do
         omniauth = request.env['omniauth.auth']
-
-        # The google oauth 2 is different because the migration of old
-        # database, since we don't had the uid...
-        # We can remove this when all users be with the uid saved.
-        if p.name == 'google_oauth2'
-            @user = User.
-            select('users.*').
-            joins('JOIN authorizations ON authorizations.user_id = users.id').
-            joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
-            where("users.email = :email AND oauth_providers.name = :provider", {email: omniauth['info']['email'], provider: p.name}).
-            first || User.create_with_omniauth(omniauth, current_user)
-
-            # Save the uid for old users that don't have it.
-            # We can remove this when all users be with the uid saved.
-            if @user
-              authorization = @user.authorizations.where(provider: 'google_oauth2').first
-              if authorization and not authorization.uid.present?
-                authorization.uid = omniauth[:uid]
-                authorization.save
-              end
-            end
-        else
-          @user = User.
-            select('users.*').
-            joins('JOIN authorizations ON authorizations.user_id = users.id').
-            joins('JOIN oauth_providers ON oauth_providers.id = authorizations.oauth_provider_id').
-            where("authorizations.uid = :uid AND oauth_providers.name = :provider", {uid: omniauth[:uid], provider: p.name}).
-            first
-
-            if @user.nil? and omniauth['info']['email']
-              @user = User.create_with_omniauth(omniauth, current_user)
-            end
-
-            if @user.nil?
-              session[:omniauth] = omniauth.except('extra')
-              return render 'users/set_email', layout: 'devise'
-            end
+        unless (@auth = Authorization.find_from_hash(omniauth))
+          user = current_user || (User.find_by_email(omniauth[:info][:email]) rescue nil)
+          @auth = omniauth[:info][:email].present? ? Authorization.create_from_hash(omniauth, user) : Authorization.create_without_email_from_hash(omniauth, user)
         end
 
-        flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: p.name.capitalize)
-        sign_in @user, event: :authentication
-        if @user.email
-          redirect_to(session[:return_to] || root_path)
-          session[:return_to] = nil
-        else
-          render 'users/set_email', layout: 'devise'
-        end
+        update_google_uid(@auth, omniauth['uid']) if omniauth['provider'] == 'google_oauth2'
+
+        sign_in @auth.user, event: :authentication
+        redirect_to(session[:return_to] || root_path, flash: { notice: flash_message(@auth.user, p.name.capitalize) })
+        session[:return_to] = nil
       end
     end
+  end
+
+  add_providers if Rails.env.development?
+
+  protected
+  def flash_message(user, kind)
+    if user.confirmed?
+      t('devise.omniauth_callbacks.success', kind: kind)
+    else
+      t('devise.confirmations.send_instructions')
+    end
+  end
+
+  def update_google_uid(auth, uid)
+    # Save the uid for old google users (openid)
+    # We can remove this when all users be with the new uid saved.
+    auth.update_attribute(:uid, uid)
   end
 end
