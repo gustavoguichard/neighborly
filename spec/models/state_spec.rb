b28cab3ab22389c8d935bc58a491fe668220c1ce
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-19 15:00:04 -0600

    Create specs for State.array

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -10,4 +10,13 @@ describe State do
     it{ should validate_presence_of(:acronym) }
     it{ should validate_uniqueness_of(:acronym).scoped_to(:name) }
   end
+
+  describe '.array' do
+    before do
+      create(:state, name: 'California', acronym: 'CA')
+      create(:state, name: 'Missouri', acronym: 'MO')
+    end
+
+    it { expect(State.array).to eq [['Select an option', ''], ['California - CA', 'CA'], ['Missouri - MO', 'MO']] }
+  end
 end
