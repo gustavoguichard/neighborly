0afa1a94ea6fa7c08216d185f7bb3bf1ea55d945
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-19 14:55:08 -0600

    Remvoe unused calendar class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,7 +111,6 @@ RSpec.configure do |config|
     CatarseMailchimp::API.stub(:unsubscribe)
     Notification.stub(:notify)
     Notification.stub(:notify_once)
-    Calendar.any_instance.stub(:fetch_events_from)
     Configuration[:base_domain] = 'localhost'
     Configuration[:email_contact] = 'foo@bar.com'
     Configuration[:company_name] = 'Foo Bar Company'
