3e7a0d268567013652e543c8fab9364a2c117bd0
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-19 14:55:29 -0600

    Fix spec user observer

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -19,9 +19,4 @@ describe UserObserver do
     end
   end
 
-  context 'before_save' do
-    subject { create(:user, twitter: '@should_be_change') }
-
-    its(:twitter) { should == 'should_be_change' }
-  end
 end
