51ff447b00ae9cc4747382fb604582ebfdf2d557
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-12-21 14:06:16 -0600

    Add option for css class on tab_link_to and add private on tabs styles

--- app/helpers/tab_link_to_helper.rb
+++ app/helpers/tab_link_to_helper.rb
@@ -1,10 +1,10 @@
 module TabLinkToHelper
-  def tab_link_to(name = nil, url = nil, &block)
+  def tab_link_to(name = nil, url = nil, klass = nil, &block)
     if block_given?
       url = name
-      link_to(url, class: " #{ 'selected' if current_page?(url) }", &block)
+      link_to(url, class: " #{ 'selected' if current_page?(url) } #{klass}", &block)
     else
-      link_to(name, url, class: " #{ 'selected' if current_page?(url) }")
+      link_to(name, url, class: " #{ 'selected' if current_page?(url) } #{klass}")
     end
   end
 end
