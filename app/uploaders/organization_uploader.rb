a48307cdc78fdcd7e21241088b2cfb727c7efd00
Josemar Luedke <josemarluedke@gmail.com>
2014-01-08 12:52:22 -0600

    Change organization uploader to use resize_and_pad

--- app/uploaders/organization_uploader.rb
+++ app/uploaders/organization_uploader.rb
@@ -3,11 +3,11 @@ class OrganizationUploader < ImageUploader
   process quality: 90
 
   version :thumb do
-    process resize_to_fill: [170, 85]
+    process resize_and_pad: [170, 85]
   end
 
   version :large do
-    process resize_to_fill: [300, 150]
+    process resize_and_pad: [300, 150]
   end
 
 end
