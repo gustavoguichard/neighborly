dc428bacbd41d913c7204cd7cfd5f408819ba1d3
Josemar Luedke <josemarluedke@gmail.com>
2014-01-24 16:28:05 -0200

    Add custom flash message to company contact

--- app/controllers/companies/contacts_controller.rb
+++ app/controllers/companies/contacts_controller.rb
@@ -2,4 +2,11 @@ class Companies::ContactsController < ApplicationController
   inherit_resources
   defaults class_name: 'CompanyContact'
   actions :new, :create
+
+  def create
+    create! do
+      flash[:notice] = { message: t('controllers.companies.contacts.create.success'), dismissible: false }
+      return redirect_to companies_contact_path
+    end
+  end
 end
