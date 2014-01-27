63496389e0f17717af41a8775b79cf720ae7a706
Josemar Luedke <josemarluedke@gmail.com>
2014-01-27 11:06:10 -0200

    Apply neighbor.ly changes to statistics view

--- db/migrate/20140121124230_migrate_statistics_view_to_contributions.rb
+++ db/migrate/20140121124230_migrate_statistics_view_to_contributions.rb
@@ -3,18 +3,36 @@ class MigrateStatisticsViewToContributions < ActiveRecord::Migration
     execute <<-SQL
        DROP VIEW statistics;
        CREATE OR REPLACE VIEW statistics AS
-       SELECT ( SELECT count(*) AS count
-               FROM users) AS total_users,
-        contributions_totals.total_backs, contributions_totals.total_contributions,
-        contributions_totals.total_backed, projects_totals.total_projects,
+       SELECT
+        ( SELECT count(*) AS count FROM users) AS total_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'organization'::text ) AS total_organization_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'personal'::text ) AS total_personal_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'channel'::text ) AS total_channel_users,
+        ( SELECT COUNT(*) FROM ( SELECT DISTINCT address_city, address_state FROM projects ) AS count ) AS total_communities,
+        contributions_totals.total_contributions,
+        contributions_totals.total_contributors,
+        contributions_totals.total_contributed,
+        projects_totals.total_projects,
         projects_totals.total_projects_success,
-        projects_totals.total_projects_online
-       FROM ( SELECT count(*) AS total_backs,
+        projects_totals.total_projects_online,
+        projects_totals.total_projects_draft,
+        projects_totals.total_projects_soon
+       FROM ( SELECT count(*) AS total_contributions,
                 count(DISTINCT contributions.user_id) AS total_contributions,
-                sum(contributions.value) AS total_backed
+                sum(contributions.value) AS total_contributed
                FROM contributions
               WHERE contributions.state::text <> ALL (ARRAY['waiting_confirmation'::character varying::text, 'pending'::character varying::text, 'canceled'::character varying::text, 'deleted'])) contributions_totals,
         ( SELECT count(*) AS total_projects,
+                count(
+                    CASE
+                        WHEN projects.state::text = 'draft'::text THEN 1
+                        ELSE NULL::integer
+                    END) AS total_projects_draft,
+              count(
+                    CASE
+                        WHEN projects.state::text = 'soon'::text THEN 1
+                        ELSE NULL::integer
+                    END) AS total_projects_soon,
                 count(
                     CASE
                         WHEN projects.state::text = 'successful'::text THEN 1
@@ -26,7 +44,7 @@ class MigrateStatisticsViewToContributions < ActiveRecord::Migration
                         ELSE NULL::integer
                     END) AS total_projects_online
                FROM projects
-              WHERE projects.state::text <> ALL (ARRAY['draft'::character varying::text, 'rejected'::character varying::text])) projects_totals;
+              WHERE projects.state::text <> ALL (ARRAY['deleted'::character varying::text, 'rejected'::character varying::text])) projects_totals;
     SQL
   end
 
@@ -34,12 +52,20 @@ class MigrateStatisticsViewToContributions < ActiveRecord::Migration
     execute <<-SQL
        DROP VIEW statistics;
        CREATE OR REPLACE VIEW statistics AS
-       SELECT ( SELECT count(*) AS count
-               FROM users) AS total_users,
-        backers_totals.total_backs, backers_totals.total_backers,
-        backers_totals.total_backed, projects_totals.total_projects,
+       SELECT
+        ( SELECT count(*) AS count FROM users) AS total_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'organization'::text ) AS total_organization_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'personal'::text ) AS total_personal_users,
+        ( SELECT count(*) AS count FROM users WHERE profile_type::text = 'channel'::text ) AS total_channel_users,
+        ( SELECT COUNT(*) FROM ( SELECT DISTINCT address_city, address_state FROM projects ) AS count ) AS total_communities,
+        backers_totals.total_backs,
+        backers_totals.total_backers,
+        backers_totals.total_backed,
+        projects_totals.total_projects,
         projects_totals.total_projects_success,
-        projects_totals.total_projects_online
+        projects_totals.total_projects_online,
+        projects_totals.total_projects_draft,
+        projects_totals.total_projects_soon
        FROM ( SELECT count(*) AS total_backs,
                 count(DISTINCT contributions.user_id) AS total_backers,
                 sum(contributions.value) AS total_backed
@@ -48,6 +74,16 @@ class MigrateStatisticsViewToContributions < ActiveRecord::Migration
         ( SELECT count(*) AS total_projects,
                 count(
                     CASE
+                        WHEN projects.state::text = 'draft'::text THEN 1
+                        ELSE NULL::integer
+                    END) AS total_projects_draft,
+              count(
+                    CASE
+                        WHEN projects.state::text = 'soon'::text THEN 1
+                        ELSE NULL::integer
+                    END) AS total_projects_soon,
+                count(
+                    CASE
                         WHEN projects.state::text = 'successful'::text THEN 1
                         ELSE NULL::integer
                     END) AS total_projects_success,
@@ -57,7 +93,7 @@ class MigrateStatisticsViewToContributions < ActiveRecord::Migration
                         ELSE NULL::integer
                     END) AS total_projects_online
                FROM projects
-              WHERE projects.state::text <> ALL (ARRAY['draft'::character varying::text, 'rejected'::character varying::text])) projects_totals;
+              WHERE projects.state::text <> ALL (ARRAY['deleted'::character varying::text, 'rejected'::character varying::text])) projects_totals;
     SQL
   end
 end
