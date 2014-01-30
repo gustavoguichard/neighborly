7298a6437c9c9c384bdb6531dddf9a9817bf43e7
Josemar Luedke <josemarluedke@gmail.com>
2014-01-30 21:23:49 -0200

    Add specs for user observer

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe UserObserver do
 
+  describe '#before_validation' do
+    let(:user) { build(:user, password: nil) }
+    before { user.valid? }
+    it { expect(user.password).to_not be_empty }
+  end
+
+  describe '#after_create' do
+    before { UserObserver.any_instance.unstub(:after_create) }
+    context 'when the user is with temporary email' do
+      let(:user) { create(:user, email: "change-your-email+#{Time.now.to_i}@neighbor.ly") }
+      before { expect_any_instance_of(WelcomeWorker).to_not receive(:perform_async) }
+      it { user }
+    end
+
+    context 'when the user is not with temporary email' do
+      before { expect(WelcomeWorker).to receive(:perform_async) }
+      it { create(:user) }
+    end
+  end
+
 end
