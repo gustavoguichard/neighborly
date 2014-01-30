d16250d6f8bd1c42dad39abe7aff0da5955b3ca8
Josemar Luedke <josemarluedke@gmail.com>
2014-01-30 19:05:30 -0200

    Create user completeness calculator

---
+++ app/models/concerns/user/completeness.rb
@@ -0,0 +1,43 @@
+module User::Completeness
+  class Calculator
+
+    attr_accessor :user
+
+    def initialize(user)
+      @user = user
+    end
+
+    def progress
+      send("calc_#{user.profile_type}_progress")
+    end
+
+    private
+    def calc_personal_progress
+      calc_progress %w[name bio address authorizations uploaded_image]
+    end
+
+    def calc_organization_progress
+      calc_progress %w[bio address authorizations], %w[name image], :organization
+    end
+
+    def calc_channel_progress
+      calc_progress %w[address other_url], %w[name description video_url image how_it_works], :channel
+    end
+
+    def calc_progress(user_fields, association_fields = [], association_name = nil)
+      progress = 0
+      user_fields.each do |field|
+        progress += 1 if user.send(field).present?
+      end
+
+      if association_name.present? && user.send(association_name).present?
+        association_fields.each do |field|
+          progress += 1 if user.send(association_name).send(field).present?
+        end
+      end
+
+      (progress / (user_fields + association_fields).size.to_f * 100).to_i
+    end
+
+  end
+end
