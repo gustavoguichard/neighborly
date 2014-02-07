1a40c2e019913a0446647251706b02e2977fbb7f
Josemar Luedke <josemarluedke@gmail.com>
2014-02-07 18:17:16 -0200

    Create base static example page

--- config/routes.rb
+++ config/routes.rb
@@ -79,6 +79,7 @@ Catarse::Application.routes.draw do
   get "/faq",                   to: "static#faq",                 as: :faq
   get "/terms",                 to: "static#terms",               as: :terms
   get "/privacy",               to: "static#privacy",             as: :privacy
+  get "/base",                  to: "static#base",                as: :base
 
   #get "/guidelines_backers",    to: "static#guidelines_backers",  as: :guidelines_backers
   get "/start",                 to: "static#start",               as: :start
