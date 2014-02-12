633ec6fcf1802ffb30c14a186d833c6a1dd41429
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-02-12 16:57:51 -0200

    Add white lines between specs on configuration_spec

--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe ::Configuration do
-
   before do
     @config = FactoryGirl.build(:configuration, name: 'a_config', value: 'a_value')
   end
 
-  it{ should validate_presence_of :name }
+  it { should validate_presence_of :name }
+
   it "should be valid from factory" do
     expect(@config).to be_valid
   end
@@ -16,6 +16,7 @@ describe ::Configuration do
       @config.save
       FactoryGirl.create(:configuration, name: 'other_config', value: 'another_value')
     end
+
     it "should get config" do
       expect(described_class[:a_config]).to eql('a_value')
     end
@@ -23,6 +24,7 @@ describe ::Configuration do
     it "should return nil when not found" do
       expect(described_class[:not_found_config]).to be_nil
     end
+
     it "should return array" do
       expect(
         described_class[:a_config, :other_config]
@@ -36,7 +38,7 @@ describe ::Configuration do
 
       it "should return the predefined value" do
         expect {
-          ::Configuration.fetch(:a_config)
+          described_class.fetch(:a_config)
         }.to_not raise_error
       end
     end
