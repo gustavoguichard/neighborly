8371b0da314ff0f75e18de971fc0b79405dfc8e8
Josemar Luedke <josemarluedke@gmail.com>
2014-02-18 16:27:05 -0300

    Migrate emails controller to not use notification type anymore

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,18 +1,46 @@
 class EmailsController < ApplicationController
   def index
     @notifications = []
+    types = [
+      :adm_project_deadline,
+      :company_contact,
+      :confirm_contribution,
+      :contribution_canceled_after_confirmed,
+      :contribution_confirmed_after_project_was_closed,
+      :contribution_project_successful,
+      :contribution_project_unsuccessful,
+      :credits_warning,
+      :in_analysis_project,
+      :new_draft_project,
+      :new_draft_project_channel,
+      :new_project_visible,
+      :new_user_registration,
+      :payment_slip,
+      :pending_contribution_project_unsuccessful,
+      :processing_payment,
+      :project_in_wainting_funds,
+      :project_owner_contribution_confirmed,
+      :project_received,
+      :project_received_channel,
+      :project_rejected,
+      :project_rejected_channel,
+      :project_success,
+      :project_unsuccessful,
+      :project_visible,
+      :project_visible_channel
+    ]
 
-    NotificationType.all.each do |type|
-      notification = Notification.where(notification_type: type).first
+    types.each do |type|
+      notification = Notification.where(template_name: type).last
       @notifications << notification if notification
     end
   end
 
   def show
     @notification = Notification.find params[:id]
-    @header = I18n.t("notifications.#{@notification.notification_type.name}.header", @notification.mail_params)
+    #header = render_to_string(template: "notifications_mailer/subjects/#{@notification.template_name}", formats: [:text, :html])
     message = OpenStruct.new
-    message.subject = I18n.t("notifications.#{@notification.notification_type.name}.subject", @notification.mail_params)
-    render "notifications_mailer/#{@notification.notification_type.name}", layout: @notification.notification_type.layout, locals: { message: message }
+    message.subject = ''
+    render "notifications_mailer/#{@notification.template_name}", layout: 'email', locals: { message: message }
   end
 end
