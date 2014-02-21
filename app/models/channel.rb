eb100d9c712950ce4a99739e56012b4d9badae76
Josemar Luedke <josemarluedke@gmail.com>
2014-02-21 01:00:33 -0300

    Add channel concerns about start content

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,8 +4,9 @@ class Channel < ActiveRecord::Base
   extend CatarseAutoHtml
   include Shared::StateMachineHelpers
   include Channel::StateMachineHandler
+  include Channel::StartContent
 
-  attr_accessible :description, :name, :permalink, :video_url, :image, :how_it_works, :accepts_projects, :submit_your_project_text, :user, :user_id, :user_attributes
+  attr_accessible :description, :name, :permalink, :video_url, :image, :how_it_works, :accepts_projects, :submit_your_project_text, :user, :user_id, :user_attributes, :start_hero_image, :start_primary_content, :start_content
 
   validates_presence_of :name, :description, :permalink, :user
   validates_uniqueness_of :permalink
