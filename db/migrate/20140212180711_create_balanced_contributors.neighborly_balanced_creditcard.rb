62b51a273377ff7062f3c6fa7dfc798b47e45787
Josemar Luedke <josemarluedke@gmail.com>
2014-03-11 19:14:13 -0300

    Remove unused fields from balanced_contributors migration

--- db/migrate/20140212180711_create_balanced_contributors.neighborly_balanced_creditcard.rb
+++ db/migrate/20140212180711_create_balanced_contributors.neighborly_balanced_creditcard.rb
@@ -4,8 +4,6 @@ class CreateBalancedContributors < ActiveRecord::Migration
     create_table :balanced_contributors do |t|
       t.references :user, index: true
       t.string :uri
-      t.string :credit_card_uri
-      t.string :bank_account_uri
 
       t.timestamps
     end
