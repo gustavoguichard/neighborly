e6f711c76eba3e4904d4065c53de446d311b6a1f
Josemar Luedke <josemarluedke@gmail.com>
2014-03-11 12:48:43 -0300

    Remove schema_associations from payment notifications

--- spec/models/payment_notification_spec.rb
+++ spec/models/payment_notification_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe PaymentNotification do
-  describe "Associations" do
-    it{ should belong_to :contribution }
+  describe 'associations' do
+    it { should belong_to :contribution }
   end
 
-  describe "#extra_data" do
-    let(:test_hash){{"test_hash" => 1}}
+  describe '#extra_data' do
+    let(:test_hash) { {"test_hash" => 1 } }
     before do
       @p = PaymentNotification.new(contribution_id: FactoryGirl.create(:contribution).id, extra_data: test_hash)
       @p.save!
     end
-    subject{ @p.extra_data }
-    it{ should == test_hash }
+    subject { @p.extra_data }
+    it { should == test_hash }
   end
 end
