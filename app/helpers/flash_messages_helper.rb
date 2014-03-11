16859fd9c1a510ea690d963be06acf0203140fc3
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-03-10 21:47:55 -0300

    Move flash messages to html helper

--- app/helpers/flash_messages_helper.rb
+++ app/helpers/flash_messages_helper.rb
@@ -1,30 +1,32 @@
 module FlashMessagesHelper
   def flash_messages
     content_tag :div, class: 'flash' do
-
       filtered_flash_messages.map do |name, message|
-        content       = message.try(:[], :message) || message
-        html_classes  = "#{name} alert-box large-10 columns large-centered animated fadeIn"
-        html_classes << ' dismissible' if !!message.try(:dismissible)
-
-        concat(content_tag(:div, class: 'row') do
-          concat(content_tag(:div, class: html_classes, 'data-alert' => 'true') do
-
-            concat(content.html_safe)
-            concat(link_to('&times;'.html_safe, nil, class: 'close'))
-          end)
-        end)
+        concat build_flash_message(name, message)
       end
-
     end if filtered_flash_messages.any?
   end
 
   private
 
-  def safe_buffer(&block)
-    buffer = ActiveSupport::SafeBuffer.new
-    yield buffer if block_given?
-    buffer
+  def build_flash_message(name, message)
+    content = message.is_a?(String) ? message : message.try(:[], :message)
+
+    content_tag(:div, class: 'row') do
+      concat(content_tag(:div,
+                         class:          html_classes_for_msg(name, message),
+                         'data-alert' => 'true') do
+
+        concat(content.html_safe)
+        concat(link_to('&times;'.html_safe, '#', class: 'close'))
+      end)
+    end
+  end
+
+  def html_classes_for_msg(name, message)
+    html_classes  = "#{name} alert-box large-10 columns large-centered animated fadeIn"
+    html_classes << ' dismissible' if !!message.try(:dismissible)
+    html_classes
   end
 
   def filtered_flash_messages
