59192ce28dbe2197995b89199cf4cc2ab03e06a5
Josemar Luedke <josemarluedke@gmail.com>
2014-03-11 23:56:36 -0300

    Add conditional to check if the table exists on initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,8 @@
 # Be sure to restart your server when you modify this file.
-
-if Rails.env.production? && Configuration[:base_domain]
-  Neighborly::Application.config.session_store :cookie_store, key: Configuration[:secret_token], domain: Configuration[:base_domain]
-else
-  Neighborly::Application.config.session_store :cookie_store, key: Configuration[:secret_token]
+if ActiveRecord::Base.connection.tables.include?(::Configuration.table_name)
+  if Rails.env.production? && Configuration[:base_domain]
+    Neighborly::Application.config.session_store :cookie_store, key: Configuration[:secret_token], domain: Configuration[:base_domain]
+  else
+    Neighborly::Application.config.session_store :cookie_store, key: Configuration[:secret_token]
+  end
 end
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Catarse::Application.config.session_store :active_record_store
-
