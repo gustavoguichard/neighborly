79955fa04951762be204d9cb3b1e0e516891f1ca
Josemar Luedke <josemarluedke@gmail.com>
2014-03-26 16:36:07 -0300

    Create project policy

---
+++ app/policies/project_policy.rb
@@ -0,0 +1,55 @@
+class ProjectPolicy < ApplicationPolicy
+  def create?
+    done_by_onwer_or_admin?
+  end
+
+  def update?
+    done_by_onwer_or_admin? || is_channel_admin?
+  end
+
+  def show?
+    if record.draft? || record.in_analysis? || record.soon?
+      done_by_onwer_or_admin? || is_channel_admin?
+    else
+      true
+    end
+  end
+
+  def send_to_analysis?
+    update?
+  end
+
+  def success?
+    update?
+  end
+
+  def reports?
+    update?
+  end
+
+  def permitted_attributes
+    if user.present? && (!record.instance_of?(Project) || fully_editable?)
+      { project: record.attribute_names.map(&:to_sym) + [:address] }
+    else
+      { project: [:about,         :video_url,     :uploaded_image, :hero_image, :headline,
+                  :budget,        :terms,         :address_neighborhood, :address,
+                  :address_city,  :address_state, :hash_tag, :site, :tag_list] }
+
+
+    end
+  end
+
+  protected
+
+  def fully_editable?
+    record.instance_of?(Project) && ( !record.persisted? ||
+                                      user.admin? ||
+                                      (record.draft? || record.rejected? ||
+                                      record.in_analysis? || record.soon?) )
+  end
+
+  def is_channel_admin?
+    user.present? && ( record.last_channel.try(:user) == user ||
+                       user.channels.include?(record.last_channel) )
+  end
+end
