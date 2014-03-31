9f81246cc113b7939a470d855d2db17c1ffb6541
Josemar Luedke <josemarluedke@gmail.com>
2014-03-31 16:28:34 -0300

    Remove payment_notification observer

--- config/application.rb
+++ config/application.rb
@@ -43,10 +43,9 @@ module Neighborly
     end
 
     config.active_record.observers = [
-      :contribution_observer, :user_observer,
-      :update_observer, :project_observer,
-      :payment_notification_observer, :company_contact_observer,
-      :authorization_observer
+      :contribution_observer,     :user_observer,
+      :update_observer,           :project_observer,
+      :company_contact_observer,  :authorization_observer
     ]
 
     # Enable the asset pipeline
