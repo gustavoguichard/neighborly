7f84ef131f8794542ddaf105a8f665475cef331b
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-03 11:48:06 -0300

    Create OmniauthUserSerializer

---
+++ app/models/omniauth_user_serializer.rb
@@ -0,0 +1,51 @@
+class OmniauthUserSerializer
+  def initialize(omniauth_data)
+    @omniauth_data = omniauth_data
+  end
+
+  def to_h
+    {
+      name:                      name,
+      email:                     email,
+      nickname:                  nickname,
+      bio:                       bio,
+      locale:                    locale,
+      authorizations_attributes: [authorization]
+    }.reject { |_key, value| value.blank? }
+  end
+
+  def authorization
+    {
+      uid:               uid,
+      oauth_provider_id: oauth_provider_id
+    }
+  end
+
+  def name
+    @omniauth_data['info']['name']
+  end
+
+  def email
+    @omniauth_data['info']['email']
+  end
+
+  def nickname
+    @omniauth_data['info']['nickname']
+  end
+
+  def bio
+    @omniauth_data['info']['description'].try(:[], 0..139)
+  end
+
+  def locale
+    I18n.locale.to_s
+  end
+
+  def uid
+    @omniauth_data['uid']
+  end
+
+  def oauth_provider_id
+    OauthProvider.find_by(name: @omniauth_data['provider']).id
+  end
+end
