67cffbfa3c764e65f30ac30e1350f3bcd4e950f4
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-04 18:36:09 -0300

    Remove from_hash scopes from Authorization
    Not used since authorization refactory.

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,12 +3,4 @@ class Authorization < ActiveRecord::Base
   belongs_to :oauth_provider
 
   validates_presence_of :oauth_provider, :uid
-
-  scope :from_hash, ->(hash){
-    joins(:oauth_provider).where("oauth_providers.name = :name AND uid = :uid", { name: hash['provider'], uid: hash['uid'] })
-  }
-
-  scope :from_hash_without_uid, ->(hash){
-    joins(:oauth_provider).where("oauth_providers.name = :name", { name: hash['provider'] }).joins(:user).where('users.email = :email', { email: hash['info']['email'] })
-  }
 end
