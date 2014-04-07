23fc6322ff2203dedd208d0e6b840ba9c36d8be9
Josemar Luedke <josemarluedke@gmail.com>
2014-04-07 13:42:03 -0300

    Import contribution policy from catarse

---
+++ app/policies/contribution_policy.rb
@@ -0,0 +1,38 @@
+class ContributionPolicy < ApplicationPolicy
+
+  self::UserScope = Struct.new(:current_user, :user, :scope) do
+    def resolve
+      if current_user.try(:admin?)
+        scope.available_to_display
+      elsif current_user == user
+        scope.with_state('confirmed')
+      else
+        scope.not_anonymous.with_state('confirmed')
+      end
+    end
+  end
+
+  def create?
+    done_by_owner_or_admin? && record.project.online?
+  end
+
+  def update?
+    done_by_owner_or_admin?
+  end
+
+  def show?
+    done_by_owner_or_admin?
+  end
+
+  def credits_checkout?
+    done_by_owner_or_admin?
+  end
+
+  def request_refund?
+    done_by_owner_or_admin?
+  end
+
+  def permitted_attributes
+    {contribution: record.attribute_names.map(&:to_sym) - %i[user_attributes user_id user payment_service_fee payment_id]}
+  end
+end
---
+++ spec/policies/contribution_policy_spec.rb
@@ -0,0 +1,95 @@
+require "spec_helper"
+
+describe ContributionPolicy do
+  subject{ ContributionPolicy }
+
+  let(:project){ create(:project) }
+  let(:contribution){ create(:contribution) }
+  let(:user){ contribution.user }
+
+  shared_examples_for "update permissions" do
+    it "should deny access if user is nil" do
+      should_not permit(nil, contribution)
+    end
+
+    it "should deny access if user is not updating his contribution" do
+      should_not permit(User.new, contribution)
+    end
+
+    it "should permit access if user is contribution owner" do
+      should permit(user, contribution)
+    end
+
+    it "should permit access if user is admin" do
+      admin = build(:user, admin: true)
+      should permit(admin, contribution)
+    end
+  end
+
+  shared_examples_for "create permissions" do
+    it_should_behave_like "update permissions"
+
+    ['draft', 'deleted', 'rejected', 'successful', 'failed', 'waiting_funds', 'in_analysis'].each do |state|
+      it "should deny access if project is #{state}" do
+        contribution.project.update_attributes state: state
+        should_not permit(user, contribution)
+      end
+    end
+  end
+
+  permissions(:new?){ it_should_behave_like "create permissions" }
+
+  permissions(:create?){ it_should_behave_like "create permissions" }
+
+  permissions(:show?){ it_should_behave_like "update permissions" }
+
+  permissions(:update?){ it_should_behave_like "update permissions" }
+
+  permissions(:edit?){ it_should_behave_like "update permissions" }
+
+  permissions(:credits_checkout?){ it_should_behave_like "update permissions" }
+
+  permissions(:request_refund?){ it_should_behave_like "update permissions" }
+
+  describe 'UserScope' do
+    describe ".resolve" do
+      let(:current_user) { create(:user, admin: false) }
+      let(:user) { nil }
+      before do
+        @contribution = create(:contribution, anonymous: false, state: 'confirmed', project: project)
+        @anon_contribution = create(:contribution, anonymous: true, state: 'confirmed', project: project)
+      end
+
+      subject { ContributionPolicy::UserScope.new(current_user, user, project.contributions).resolve.order('created_at desc') }
+
+      context "when user is admin" do
+        let(:current_user) { create(:user, admin: true) }
+
+        it { should have(2).itens }
+      end
+
+      context "when user is a contributor" do
+        let(:current_user) { user }
+        it { should eq [@anon_contribution, @contribution] }
+      end
+
+      context "when user is not an admin" do
+        it { should eq [@contribution] }
+      end
+    end
+  end
+
+  describe "#permitted?" do
+    let(:policy){ ContributionPolicy.new(user, build(:contribution)) }
+    subject{ policy }
+
+    %i[user_attributes user_id user payment_service_fee payment_id].each do |field|
+      it{ should_not be_permitted(field) }
+    end
+
+    %i[value].each do |field|
+      it{ should be_permitted(field) }
+    end
+  end
+
+end
