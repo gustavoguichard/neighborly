586a12b3bf03c0a5fefef20841cfc886ade420b1
Josemar Luedke <josemarluedke@gmail.com>
2014-04-07 12:16:16 -0300

    Migrate rewards controller to pundit

--- app/policies/reward_policy.rb
+++ app/policies/reward_policy.rb
@@ -17,14 +17,18 @@ class RewardPolicy < ApplicationPolicy
 
   def permitted_attributes
     attributes = record.attribute_names.map(&:to_sym)
-    attributes.delete(:minimum_value) unless not_sold_yet?
-    attributes.delete(:days_to_delivery) if project_finished?
+
+    if record.instance_of?(Reward)
+      attributes.delete(:minimum_value) unless not_sold_yet?
+      attributes.delete(:days_to_delivery) if project_finished?
+    end
+
     { reward: attributes }
   end
 
   protected
   def not_sold_yet?
-    record.total_compromised == 0
+    record.try(:total_compromised) == 0
   end
 
   def project_finished?
