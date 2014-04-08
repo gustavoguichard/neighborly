fd836b4bf519b7b573731205b2b8f82fc7995c25
Josemar Luedke <josemarluedke@gmail.com>
2014-04-08 10:36:28 -0300

    Migrate project documents controller to pundit

--- app/controllers/projects/terms_controller.rb
+++ app/controllers/projects/terms_controller.rb
@@ -1,7 +1,7 @@
 class Projects::TermsController < ApplicationController
+  after_filter :verify_authorized, except: :index
   inherit_resources
   defaults resource_class: ProjectDocument, collection_name: :project_documents
-  load_and_authorize_resource class: :ProjectDocument
 
   actions :create, :destroy
   belongs_to :project, finder: :find_by_permalink!
@@ -12,10 +12,19 @@ class Projects::TermsController < ApplicationController
   end
 
   def create
+    @project_document = ProjectDocument.new(permitted_params[:project_document].
+                                            merge(project: parent))
+    authorize @project_document
     create! { project_terms_path(parent) }
   end
 
   def destroy
+    authorize resource
     destroy! { project_terms_path(parent) }
+  end
+
+  private
+  def permitted_params
+    params.permit(policy(@project_document || ProjectDocument).permitted_attributes)
   end
 end
