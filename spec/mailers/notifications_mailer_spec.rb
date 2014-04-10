b75280bd72dfeb565d653c816888742764296315
Josemar Luedke <josemarluedke@gmail.com>
2014-04-10 10:46:23 -0300

    Add option to add bcc to a notification on mailers

--- spec/mailers/notifications_mailer_spec.rb
+++ spec/mailers/notifications_mailer_spec.rb
@@ -1,21 +1,51 @@
 require 'spec_helper'
 
 describe NotificationsMailer do
-  describe ".notify" do
-    let(:notification){ create(:notification, template_name: 'confirm_contribution', user: create(:user, locale: 'pt'), origin_name: 'Catarse', origin_email: 'contact@foo.bar') }
-    subject{ NotificationsMailer.notify(notification) }
+  subject { described_class.notify(notification) }
 
-    before do
-      notification
-      Mail::Message.any_instance.stub(:deliver)
-      NotificationsMailer.any_instance.should_receive(:mail).with({
-        from: "Catarse <contact@foo.bar>",
-        to: notification.user.email,
-        subject: 'Thank you for your contribution to Foo bar!',
-        template_name: 'confirm_contribution'
-      })
+  describe '.notify' do
+    context 'when there is no bcc' do
+      let(:notification){ create(:notification,
+                                 template_name: 'confirm_contribution',
+                                 user: create(:user, locale: 'pt'),
+                                 origin_name: 'Catarse',
+                                 origin_email: 'contact@foo.bar') }
+
+      before do
+        notification
+        Mail::Message.any_instance.stub(:deliver)
+        described_class.any_instance.should_receive(:mail).with({
+          from: 'Catarse <contact@foo.bar>',
+          to: notification.user.email,
+          subject: 'Thank you for your contribution to Foo bar!',
+          template_name: 'confirm_contribution'
+        })
+      end
+
+      it('should satisfy expectations') { subject }
     end
 
-    it("should satisfy expectations"){ subject }
+    context 'when there is a bcc' do
+      let(:notification){ create(:notification,
+                                 template_name: 'confirm_contribution',
+                                 user: create(:user, locale: 'pt'),
+                                 origin_name: 'Catarse',
+                                 origin_email: 'contact@foo.bar',
+                                 bcc: 'test@bcc.com') }
+
+      before do
+        notification
+        Mail::Message.any_instance.stub(:deliver)
+        described_class.any_instance.should_receive(:mail).with({
+          from: 'Catarse <contact@foo.bar>',
+          to: notification.user.email,
+          subject: 'Thank you for your contribution to Foo bar!',
+          template_name: 'confirm_contribution',
+          bcc: 'test@bcc.com'
+        })
+      end
+
+      it('should satisfy expectations') { subject }
+    end
   end
 end
