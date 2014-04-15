a54adf339ebc6c0b9d9ef7a7e0cbc2ea79f84546
Josemar Luedke <josemarluedke@gmail.com>
2014-04-15 11:44:28 -0300

    Remove double quotes and change should to expect on project observer spec

--- spec/observers/project_observer_spec.rb
+++ spec/observers/project_observer_spec.rb
@@ -13,12 +13,12 @@ describe ProjectObserver do
     Configuration[:email_contact] = 'foo@foo.com'
     Configuration[:facebook_url] = 'http://facebook.com/foo'
     Configuration[:blog_url] = 'http://blog.com/foo'
-    Configuration[:company_name] = 'Catarse'
+    Configuration[:company_name] = 'Neighbor.ly'
     Notification.unstub(:notify)
     Notification.unstub(:notify_once)
   end
 
-  describe "after_create" do
+  describe 'after_create' do
     let(:user) { create(:user, email: ::Configuration[:email_projects])}
     before do
       ProjectObserver.any_instance.should_receive(:after_create).and_call_original
@@ -26,27 +26,27 @@ describe ProjectObserver do
       project
     end
 
-    it "should create notification for project owner" do
-      Notification.where(user_id: project.user.id, template_name: 'project_received', project_id: project.id).first.should_not be_nil
+    it 'should create notification for project owner' do
+      expect(Notification.where(user_id: project.user.id, template_name: 'project_received', project_id: project.id).first).not_to be_nil
     end
 
-    it "should create notification for catarse admin" do
-      Notification.where(user_id: user.id, template_name: :new_draft_project, project_id: project.id).first.should_not be_nil
+    it 'should create notification for catarse admin' do
+      expect(Notification.where(user_id: user.id, template_name: :new_draft_project, project_id: project.id).first).not_to be_nil
     end
   end
 
-  describe "before_save" do
+  describe 'before_save' do
     let(:channel){ create(:channel) }
     let(:project){ create(:project, video_url: 'http://vimeo.com/11198435', state: 'draft')}
 
-    context "when project is launched and belongs to a channel" do
+    context 'when project is launched and belongs to a channel' do
       let(:project){ create(:project, video_url: 'http://vimeo.com/11198435', state: 'draft', channels: [channel])}
       before do
         project.update_attributes state: 'draft'
       end
 
-      it "should call notify using channel data" do
-        Notification.should_receive(:notify_once).with(
+      it 'should call notify using channel data' do
+        expect(Notification).to receive(:notify_once).with(
           :project_visible_channel,
           project.user,
           { project_id: project.id, channel_id: channel.id},
@@ -61,14 +61,14 @@ describe ProjectObserver do
       end
     end
 
-    context "when project is launched" do
+    context 'when project is launched' do
       before do
         project.update_attributes state: 'draft'
-        ProjectDownloaderWorker.should_receive(:perform_async).with(project.id).never
+        expect(ProjectDownloaderWorker).to receive(:perform_async).with(project.id).never
       end
 
-      it "should call notify and do not call download_video_thumbnail" do
-        Notification.should_receive(:notify_once).with(
+      it 'should call notify and do not call download_video_thumbnail' do
+        expect(Notification).to receive(:notify_once).with(
           :project_visible,
           project.user,
           { project_id: project.id, channel_id: nil},
@@ -83,27 +83,27 @@ describe ProjectObserver do
       end
     end
 
-    context "when video_url changes" do
+    context 'when video_url changes' do
       before do
-        ProjectDownloaderWorker.should_receive(:perform_async).with(project.id).at_least(1)
-        Notification.should_receive(:notify).never
-        Notification.should_receive(:notify_once).never
+        expect(ProjectDownloaderWorker).to receive(:perform_async).with(project.id).at_least(1)
+        expect(Notification).to receive(:notify).never
+        expect(Notification).to receive(:notify_once).never
       end
 
-      it "should call project downloader service and do not call create_notification" do
+      it 'should call project downloader service and do not call create_notification' do
         project.video_url = 'http://vimeo.com/66698435'
         project.save!
       end
     end
   end
 
-  describe "#notify_owner_that_project_is_waiting_funds" do
+  describe '#notify_owner_that_project_is_waiting_funds' do
     let(:user) { create(:user) }
     let(:project) { create(:project, user: user, goal: 100, online_days: -2, state: 'online') }
 
     before do
       create(:contribution, project: project, value: 200, state: 'confirmed')
-      Notification.should_receive(:notify_once).with(
+      expect(Notification).to receive(:notify_once).with(
         :project_in_wainting_funds,
         project.user,
         {project_id: project.id},
@@ -114,10 +114,10 @@ describe ProjectObserver do
       )
     end
 
-    it("should notify the project owner"){ project.finish }
+    it('should notify the project owner'){ project.finish }
   end
 
-  describe "sync with mailchimp" do
+  describe 'sync with mailchimp' do
     before do
       Configuration[:mailchimp_successful_projects_list] = 'OwnerListId'
       Configuration[:mailchimp_failed_projects_list] = 'UnsuccesfulListId'
@@ -133,7 +133,7 @@ describe ProjectObserver do
       end
 
       it 'subscribe project owner to successful projects mailchimp list' do
-        CatarseMailchimp::API.should_receive(:subscribe).with({ EMAIL: user.email, FNAME: user.name,
+        expect(CatarseMailchimp::API).to receive(:subscribe).with({ EMAIL: user.email, FNAME: user.name,
           CITY: user.address_city, STATE: user.address_state }, 'OwnerListId')
       end
 
@@ -142,7 +142,7 @@ describe ProjectObserver do
 
     context 'when project is unsuccesful' do
       it 'subscribe project owner to failed projects mailchimp list' do
-        CatarseMailchimp::API.should_receive(:subscribe).with({ EMAIL: user.email, FNAME: user.name,
+        expect(CatarseMailchimp::API).to receive(:subscribe).with({ EMAIL: user.email, FNAME: user.name,
           CITY: user.address_city, STATE: user.address_state }, 'UnsuccesfulListId')
       end
 
@@ -150,36 +150,36 @@ describe ProjectObserver do
     end
   end
 
-  describe "notify_contributors" do
+  describe 'notify_contributors' do
 
-    context "when project is successful" do
+    context 'when project is successful' do
       let(:project){ create(:project, goal: 30, online_days: -7, state: 'online') }
       let(:contribution){ create(:contribution, key: 'should be updated', payment_method: 'should be updated', state: 'confirmed', confirmed_at: Time.now, value: 30, project: project) }
 
       before do
         contribution
         project.update_attributes state: 'waiting_funds'
-        Notification.should_receive(:notify_once).at_least(:once)
+        expect(Notification).to receive(:notify_once).at_least(:once)
         contribution.save!
         project.finish!
       end
-      it("should notify the project contributions"){ subject }
+      it('should notify the project contributions'){ subject }
     end
 
-    context "when project is unsuccessful" do
+    context 'when project is unsuccessful' do
       let(:project){ create(:project, goal: 30, online_days: -7, state: 'online') }
       let(:contribution){ create(:contribution, key: 'should be updated', payment_method: 'should be updated', state: 'confirmed', confirmed_at: Time.now, value: 20) }
       before do
         contribution
         project.update_attributes state: 'waiting_funds'
-        Notification.should_receive(:notify_once).at_least(:once)
+        expect(Notification).to receive(:notify_once).at_least(:once)
         contribution.save!
         project.finish!
       end
-      it("should notify the project contributions and owner"){ subject }
+      it('should notify the project contributions and owner'){ subject }
     end
 
-    context "when project is unsuccessful with pending contributions" do
+    context 'when project is unsuccessful with pending contributions' do
       let(:project){ create(:project, goal: 30, online_days: -7, state: 'online') }
 
       before do
@@ -189,10 +189,10 @@ describe ProjectObserver do
       end
 
       before do
-        Notification.should_receive(:notify_once).at_least(3)
+        expect(Notification).to receive(:notify_once).at_least(3)
         project.finish!
       end
-      it("should notify the project contributions and owner"){ subject }
+      it('should notify the project contributions and owner'){ subject }
     end
 
   end
@@ -206,62 +206,62 @@ describe ProjectObserver do
       project.finish
     end
 
-    it "should create notification for project owner" do
-      Notification.where(user_id: project.user.id, template_name: 'project_success', project_id: project.id).first.should_not be_nil
+    it 'should create notification for project owner' do
+      expect(Notification.where(user_id: project.user.id, template_name: 'project_success', project_id: project.id).first).not_to be_nil
     end
   end
 
-  describe "#notify_owner_that_project_is_online" do
+  describe '#notify_owner_that_project_is_online' do
     let(:project) { create(:project, state: 'draft') }
 
-    context "when project don't belong to any channel" do
+    context 'when project don\'t belong to any channel' do
       before do
         project.launch!
       end
 
-      it "should create notification for project owner" do
-        Notification.where(user_id: project.user.id, template_name: 'project_visible', project_id: project.id).first.should_not be_nil
+      it 'should create notification for project owner' do
+        expect(Notification.where(user_id: project.user.id, template_name: 'project_visible', project_id: project.id).first).not_to be_nil
       end
     end
 
-    context "when project belong to a channel" do
+    context 'when project belong to a channel' do
       before do
         project.channels << channel
         project.launch!
       end
 
-      it "should create notification for project owner" do
-        Notification.where(user_id: project.user.id, template_name: 'project_visible_channel', project_id: project.id).first.should_not be_nil
+      it 'should create notification for project owner' do
+        expect(Notification.where(user_id: project.user.id, template_name: 'project_visible_channel', project_id: project.id).first).not_to be_nil
       end
     end
   end
 
-  describe "#notify_owner_that_project_is_rejected" do
+  describe '#notify_owner_that_project_is_rejected' do
     let(:project){ create(:project, state: 'draft') }
 
-    context "when project don't belong to any channel" do
+    context 'when project don\'t belong to any channel' do
       before do
         project.reject
       end
-      it "should create notification for project owner" do
-        Notification.where(user_id: project.user.id, template_name: 'project_rejected', project_id: project.id).first.should_not be_nil
+      it 'should create notification for project owner' do
+        expect(Notification.where(user_id: project.user.id, template_name: 'project_rejected', project_id: project.id).first).not_to be_nil
       end
     end
 
-    context "when project belong to a channel" do
+    context 'when project belong to a channel' do
       before do
         project.channels << channel
         project.reject
       end
 
-      it "should create notification for project owner" do
-        Notification.where(user_id: project.user.id, template_name: 'project_rejected_channel', project_id: project.id).first.should_not be_nil
+      it 'should create notification for project owner' do
+        expect(Notification.where(user_id: project.user.id, template_name: 'project_rejected_channel', project_id: project.id).first).not_to be_nil
       end
     end
 
   end
 
-  describe "#notify_admin_that_project_reached_deadline" do
+  describe '#notify_admin_that_project_reached_deadline' do
     let(:project){ create(:project, goal: 30, online_days: -7, state: 'waiting_funds') }
     let(:user) { create(:user, email: 'foo@foo.com')}
     before do
@@ -273,8 +273,8 @@ describe ProjectObserver do
       project.finish
     end
 
-    it "should create notification for admin" do
-      Notification.where(user_id: user.id, template_name: 'adm_project_deadline', project_id: project.id).first.should_not be_nil
+    it 'should create notification for admin' do
+      expect(Notification.where(user_id: user.id, template_name: 'adm_project_deadline', project_id: project.id).first).not_to be_nil
     end
 
   end
