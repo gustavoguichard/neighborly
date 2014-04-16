62ab966fb109c008ce72dda4ed0bb82b93120586
Josemar Luedke <josemarluedke@gmail.com>
2014-04-16 10:20:41 -0300

    Move authentication method from application controller to a concern

---
+++ app/controllers/concerns/authentication_handler.rb
@@ -0,0 +1,53 @@
+module Concerns::AuthenticationHandler
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :require_basic_auth
+    before_filter :set_return_to, if: -> { !current_user && params[:redirect_to].present? }
+    before_filter :redirect_user_back_after_login, unless: :devise_controller?
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    private
+    def set_return_to
+      if params[:redirect_to].present?
+        session[:return_to] = params[:redirect_to]
+        flash[:devise_error] = t('devise.failure.unauthenticated')
+      end
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      return_to = session[:return_to]
+      session[:return_to] = nil
+      (return_to || root_path)
+    end
+
+    def redirect_user_back_after_login
+      if request.env['REQUEST_URI'].present? && !request.xhr?
+        session[:return_to] = request.env['REQUEST_URI']
+      end
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:name, :email, :password, :newsletter)
+      end
+    end
+
+    def require_basic_auth
+      if request.url.match Regexp.new(black_list_domains.join("|"))
+        authenticate_or_request_with_http_basic do |username, password|
+          username == 'admin' && password == 'Streetcar4321'
+        end
+      end
+    end
+
+    def black_list_domains
+      ['neighborly-staging.herokuapp.com',
+       'staging.neighbor.ly',
+        'channel.staging.neighbor.ly',
+        'kaboom.neighbor.ly',
+        'cfg.neighbor.ly',
+        'makeitright.neighbor.ly']
+    end
+  end
+end
