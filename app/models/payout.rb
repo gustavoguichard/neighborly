6eed54843759d77611496b447acff158f1df7879
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-18 14:49:58 -0300

    Use class << self to group class methods

--- app/models/payout.rb
+++ app/models/payout.rb
@@ -2,15 +2,17 @@ class Payout < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  def self.complete(project_id, requestor)
-    payout_handlers.map do |handler|
-      handler.new(project_id, requestor).complete
+  class << self
+    def complete(project_id, requestor_id)
+      payout_handlers.map do |handler|
+        handler.new(project_id, requestor_id).complete
+      end
     end
-  end
 
-  def self.payout_handlers
-    PaymentEngine.all.map do |engine|
-      engine.payout_class
-    end.uniq
+    def payout_handlers
+      PaymentEngine.all.map do |engine|
+        engine.payout_class
+      end.uniq
+    end
   end
 end
--- app/workers/payout_worker.rb
+++ app/workers/payout_worker.rb
@@ -2,7 +2,7 @@ class PayoutWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(project_id, requestor)
-    Payout.complete(project_id, requestor)
+  def perform(project_id, requestor_id)
+    Payout.complete(project_id, requestor_id)
   end
 end
