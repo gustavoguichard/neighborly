5646184e2e09f89ad9361d309d7b8b9c3a7090a0
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-18 10:57:09 -0300

    Add manual attribute to payouts db table

--- db/migrate/20140411153421_create_payouts.rb
+++ db/migrate/20140411153421_create_payouts.rb
@@ -2,9 +2,10 @@ class CreatePayouts < ActiveRecord::Migration
   def change
     create_table :payouts do |t|
       t.string     :payment_service
-      t.references :project,           index: true, null: false
-      t.references :user,              index: true
-      t.integer    :value,   limit: 8,              null: false
+      t.references :project,         index: true, null: false
+      t.references :user,            index: true
+      t.decimal    :value,                        null: false
+      t.boolean    :manual,                                    default: false
 
       t.timestamps
     end
