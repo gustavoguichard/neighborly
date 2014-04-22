1d9e13d5651726c9091aa266fc96c081052be4dd
Josemar Luedke <josemarluedke@gmail.com>
2014-04-22 14:20:38 -0300

    Remove if statement on welcome and notification worker

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -5,10 +5,8 @@ class NotificationWorker
   def perform(notification_id)
     resource = Notification.find(notification_id)
 
-    if resource
-      NotificationsMailer.notify(resource).deliver
-      resource.update_attributes dismissed: true
-    end
+    NotificationsMailer.notify(resource).deliver
+    resource.update_attributes dismissed: true
   rescue ActiveRecord::RecordNotFound
     raise "Notification #{notification_id} not found.. sending to retry queue"
   end
--- app/workers/welcome_worker.rb
+++ app/workers/welcome_worker.rb
@@ -5,10 +5,8 @@ class WelcomeWorker
   def perform(user_id)
     resource = User.find(user_id)
 
-    if resource
-      Notification.notify_once(:new_user_registration, resource, {user_id: resource.id})
-      resource.update_attribute(:newsletter, true)
-    end
+    Notification.notify_once(:new_user_registration, resource, {user_id: resource.id})
+    resource.update_attribute(:newsletter, true)
   rescue ActiveRecord::RecordNotFound
     raise "User #{user_id} not found.. sending to retry queue"
   end
