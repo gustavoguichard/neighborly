2e8231a1e3f621bb614f815d68eb35ee58ba5213
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-23 18:08:09 -0300

    Validate start and finish dates of Match

--- app/models/projects/challenges/match.rb
+++ app/models/projects/challenges/match.rb
@@ -2,5 +2,36 @@ class Projects::Challenges::Match < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
+  validates :project, :user, presence: true
   validates :maximum_value, numericality: { greater_than_or_equal_to: 1_000 }
+  validate :start_and_finish_dates
+
+  private
+
+  def start_and_finish_dates
+    ensure_starts_at_in_active_period_of_project
+    ensure_finishes_at_after_starts_at_and_in_active_period_of_project
+  end
+
+  def ensure_starts_at_in_active_period_of_project
+    unless project &&
+      (Time.zone.now.to_i..project.expires_at.to_i).include?(starts_at.to_i)
+
+      errors.add(
+        :starts_at,
+        I18n.t('activerecord.errors.models.match.attributes.starts_at.must_be_in_active_period_of_project')
+      )
+    end
+  end
+
+  def ensure_finishes_at_after_starts_at_and_in_active_period_of_project
+    unless project &&
+      (starts_at.to_i..project.expires_at.to_i).include?(finishes_at.to_i)
+
+      errors.add(
+        :finishes_at,
+        I18n.t('activerecord.errors.models.match.attributes.finishes_at.must_be_after_starts_at_and_in_active_period_of_project')
+      )
+    end
+  end
 end
