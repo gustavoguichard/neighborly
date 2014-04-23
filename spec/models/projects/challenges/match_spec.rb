1990150242372b33dcae3f729d01598adc286f16
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-23 16:22:13 -0300

    Require minimum Match#maximum_value

--- spec/models/projects/challenges/match_spec.rb
+++ spec/models/projects/challenges/match_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Projects::Challenges::Match do
   it { should belong_to(:project) }
   it { should belong_to(:user) }
+
+  describe 'validations' do
+    it { should validate_numericality_of(:maximum_value).is_greater_than_or_equal_to(1_000) }
+  end
 end
