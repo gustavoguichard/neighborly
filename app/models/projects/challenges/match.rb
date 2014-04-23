29ecfddee66f2d76c82330cee48b70d464235ffa
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-23 16:22:13 -0300

    Require minimum Match#maximum_value

--- app/models/projects/challenges/match.rb
+++ app/models/projects/challenges/match.rb
@@ -1,4 +1,6 @@
 class Projects::Challenges::Match < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  validates :maximum_value, numericality: { greater_than_or_equal_to: 1_000 }
 end
