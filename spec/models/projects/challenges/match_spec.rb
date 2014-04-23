f28e11529d42b34891d08e29f2f37459fd503c51
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-23 18:12:01 -0300

    Group date specs of match

--- spec/models/projects/challenges/match_spec.rb
+++ spec/models/projects/challenges/match_spec.rb
@@ -10,43 +10,45 @@ describe Projects::Challenges::Match do
     it { should validate_presence_of(:project) }
     it { should validate_presence_of(:user) }
 
-    it 'returns error with starts_at before current date' do
-      subject.starts_at = -1.day.from_now
-      expect(subject).to have(1).errors_on(:starts_at)
-    end
+    describe 'start and finish dates' do
+      it 'returns error with starts_at before current date' do
+        subject.starts_at = -1.day.from_now
+        expect(subject).to have(1).errors_on(:starts_at)
+      end
 
-    it 'returns no errors with starts_at between current_date and project\'s expiration date' do
-      project = Project.new
-      project.stub(:expires_at).and_return(10.days.from_now)
-      subject.project   = project
+      it 'returns no errors with starts_at between current_date and project\'s expiration date' do
+        project = Project.new
+        project.stub(:expires_at).and_return(10.days.from_now)
+        subject.project   = project
 
-      subject.starts_at = project.expires_at - 3.days
-      expect(subject).to have(:no).errors_on(:starts_at)
-    end
+        subject.starts_at = project.expires_at - 3.days
+        expect(subject).to have(:no).errors_on(:starts_at)
+      end
 
-    it 'returns error with finishes_at before starts_at' do
-      subject.starts_at   = 5.days.from_now
-      subject.finishes_at = 1.day.from_now
-      expect(subject).to have(1).errors_on(:finishes_at)
-    end
+      it 'returns error with finishes_at before starts_at' do
+        subject.starts_at   = 5.days.from_now
+        subject.finishes_at = 1.day.from_now
+        expect(subject).to have(1).errors_on(:finishes_at)
+      end
 
-    it 'returns error with finishes_at after project\'s expiration date' do
-      project = Project.new
-      project.stub(:expires_at).and_return(1.day.from_now)
-      subject.project     = project
+      it 'returns error with finishes_at after project\'s expiration date' do
+        project = Project.new
+        project.stub(:expires_at).and_return(1.day.from_now)
+        subject.project     = project
 
-      subject.finishes_at = project.expires_at + 1.day
-      expect(subject).to have(1).errors_on(:finishes_at)
-    end
+        subject.finishes_at = project.expires_at + 1.day
+        expect(subject).to have(1).errors_on(:finishes_at)
+      end
 
-    it 'returns no errors with finishes_at between starts_at and project\'s expiration date' do
-      project = Project.new
-      project.stub(:expires_at).and_return(10.days.from_now)
-      subject.project   = project
+      it 'returns no errors with finishes_at between starts_at and project\'s expiration date' do
+        project = Project.new
+        project.stub(:expires_at).and_return(10.days.from_now)
+        subject.project   = project
 
-      subject.starts_at   = project.expires_at - 8.days
-      subject.finishes_at = subject.starts_at + 1.day
-      expect(subject).to have(:no).errors_on(:finishes_at)
+        subject.starts_at   = project.expires_at - 8.days
+        subject.finishes_at = subject.starts_at + 1.day
+        expect(subject).to have(:no).errors_on(:finishes_at)
+      end
     end
   end
 end
