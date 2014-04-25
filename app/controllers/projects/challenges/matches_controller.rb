3e323c437e8ddd88607f80468d22bad868895eb5
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-25 15:20:12 -0300

    Add controller action to create match

--- app/controllers/projects/challenges/matches_controller.rb
+++ app/controllers/projects/challenges/matches_controller.rb
@@ -3,7 +3,35 @@ class Projects::Challenges::MatchesController < ApplicationController
   actions :new, :create
   helper_method :parent
 
+  def create
+    @match = Projects::Challenges::Match.new(
+      match_params.except(:starts_at, :finishes_at).
+        merge(project: parent, user: current_user)
+    )
+    @match.starts_at   = Date.strptime(
+      match_params[:starts_at], '%m/%d/%y'
+    ).in_time_zone
+    @match.finishes_at = Date.strptime(
+      match_params[:finishes_at], '%m/%d/%y'
+    ).in_time_zone
+
+    create! { parent }
+  end
+
   def parent
     @project ||= Project.find_by(permalink: params[:project_id])
+  end
+
+  protected
+
+  def match_params
+    params.require(:projects_challenges_match).permit(
+      %i(
+        starts_at
+        finishes_at
+        value
+        maximum_value
+      )
+    )
   end
 end
--- spec/controllers/projects/challenges/matches_controller_spec.rb
+++ spec/controllers/projects/challenges/matches_controller_spec.rb
@@ -1,12 +1,44 @@
 require 'spec_helper'
 
 describe Projects::Challenges::MatchesController do
+  let(:project) { create(:project, state: :online, online_date: Date.current, online_days: 30) }
+  let(:user)    { create(:user) }
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  before { sign_in(user) }
+
+  describe 'GET \'new\'' do
+    it 'assigns match variable' do
+      get :new, project_id: project.to_param
+      expect(assigns(:match)).to_not be_nil
+    end
+
+    it 'renders match template' do
+      get :new, project_id: project.to_param
+      expect(response).to render_template('projects/challenges/matches/new')
     end
   end
 
+  describe 'POST \'create\'' do
+    let(:params) do
+      {
+        "projects_challenges_match" => {
+          "value"         => "3",
+          "starts_at"     => Date.tomorrow.to_time.strftime('%m/%d/%y'),
+          "finishes_at"   => (Date.tomorrow + 2.days).to_time.strftime('%m/%d/%y'),
+          "maximum_value" => "9999"
+        }
+      }
+    end
+
+    it 'creates a new match' do
+      expect {
+        post :create, params.merge(project_id: project.to_param)
+      }.to change(Projects::Challenges::Match, :count).by(1)
+    end
+
+    it 'redirects to project page' do
+      post :create, params.merge(project_id: project.to_param)
+      expect(response).to redirect_to(project)
+    end
+  end
 end
