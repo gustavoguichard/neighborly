e4f54c328c8ffb4624426689345dd7d976a7187c
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-04-30 14:55:57 -0300

    Group belongs_to specs in a associations describe

--- spec/models/projects/challenges/match_spec.rb
+++ spec/models/projects/challenges/match_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Projects::Challenges::Match do
-  it { should belong_to(:project) }
-  it { should belong_to(:user) }
+  describe 'associations' do
+    it { should belong_to(:project) }
+    it { should belong_to(:user) }
+  end
 
   describe 'validations' do
     it { should validate_numericality_of(:maximum_value).is_greater_than_or_equal_to(1_000) }
