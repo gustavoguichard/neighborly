be9c83749432e89afe869918b391c26cf5b805c9
Josemar Luedke <josemarluedke@gmail.com>
2014-05-14 16:57:49 -0300

    Remove credits mailer

--- app/mailers/credits_mailer.rb
+++
@@ -1,9 +0,0 @@
-class CreditsMailer < ActionMailer::Base
-  include ERB::Util
-
-  def request_refund_from(contribution)
-    @contribution = contribution
-    @user = contribution.user
-    mail(from: "#{@user.name} <#{@user.email}>", to: ::Configuration[:email_payments], subject: I18n.t('credits_mailer.request_refund_from.subject', name: @contribution.project.name))
-  end
-end
