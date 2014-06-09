d3bca7d16210831fb73e10d25aefa07d44578d0d
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-06-09 16:12:41 +0100

    Remove check for existence of configuration in monkeymail initializer

--- config/initializers/catarse_monkeymail.rb
+++ config/initializers/catarse_monkeymail.rb
@@ -1,6 +1,4 @@
-if ActiveRecord::Base.connection.tables.include?(::Configuration.table_name)
-  CatarseMonkeymail.configure do |config|
-    config.api_key = ::Configuration[:mailchimp_api_key]
-    config.list_id = ::Configuration[:mailchimp_list_id]
-  end
+CatarseMonkeymail.configure do |config|
+  config.api_key = ::Configuration[:mailchimp_api_key]
+  config.list_id = ::Configuration[:mailchimp_list_id]
 end
