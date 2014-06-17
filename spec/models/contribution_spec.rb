7d77f6187c800faec478426fa061727c7c455f11
Josemar Luedke <josemarluedke@gmail.com>
2014-06-17 21:40:38 +0200

    Remove contribution scopes that is only used on admin

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -43,19 +43,6 @@ describe Contribution do
     it { should have(7).items }
   end
 
-  describe '.between_values' do
-    let(:start_at) { 10 }
-    let(:ends_at) { 20 }
-    subject { Contribution.between_values(start_at, ends_at) }
-    before do
-      create(:contribution, value: 10)
-      create(:contribution, value: 15)
-      create(:contribution, value: 20)
-      create(:contribution, value: 21)
-    end
-    it { should have(3).itens }
-  end
-
   describe '.can_cancel' do
     subject { Contribution.can_cancel}
 
