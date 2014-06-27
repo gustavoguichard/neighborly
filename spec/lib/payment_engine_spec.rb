6347797e3c0f65f4a2db7209a30da4cb7afd2b3b
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-06-27 13:00:48 +0200

    Expose interface to payment engines search payments using match_id

--- spec/lib/payment_engine_spec.rb
+++ spec/lib/payment_engine_spec.rb
@@ -41,10 +41,27 @@ describe PaymentEngine do
   end
 
   describe '.find_payment' do
-    subject { PaymentEngine.find_payment({ id: contribution.id }) }
+    it 'filters by contribution attributes' do
+      expect(
+        described_class.find_payment(id: contribution.id)
+      ).to eql(contribution)
+    end
 
-    it 'should return the correct contribution' do
-      should == contribution
+    it 'accepts filter by :contribution_id key' do
+      expect(
+        described_class.find_payment(contribution_id: contribution.id)
+      ).to eql(contribution)
+    end
+
+    it 'accepts filter by :match_id key' do
+      match = create(:match)
+      expect(
+        described_class.find_payment(match_id: match.id)
+      ).to eql(match)
+    end
+
+    it 'returns nil when called with no filter' do
+      expect(described_class.find_payment({})).to be_nil
     end
   end
 
