df3918a6c0e138cf975f10245cf28a971963c232
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-07-10 15:15:21 +0200

    Keep definition of `#large` variable in view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,12 +1,11 @@
 module ProjectsHelper
-  def project_box_classes(project)
-    columns_n = if defined? columns then columns else 'large-3 medium-4' end
-
-    classes = if large_project?(project)
+  def project_box_classes(project, is_large, contribution, columns)
+    classes = if is_large
       'large large-9 medium-8 columns'
-    elsif defined?(contribution) && !browser.mobile?
+    elsif contribution && !browser.mobile?
       'large large-12 medium-12 columns'
     else
+      columns_n = columns || 'large-3 medium-4'
       "#{columns_n} columns"
     end
     classes << " #{project.category.to_s.parameterize}" if project.category
@@ -15,15 +14,11 @@ module ProjectsHelper
     classes
   end
 
-  def project_content_classes(project)
-    if large_project?(project)
+  def project_content_classes(project, is_large, contribution)
+    if is_large
       'large-4 medium-4 columns right'
-    elsif defined?(contribution) && !browser.mobile?
+    elsif contribution && !browser.mobile?
       'large-3 medium-3 columns'
     end
-  end
-
-  def large_project?(project)
-    defined?(large) && large
   end
 end
