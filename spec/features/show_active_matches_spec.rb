e7783d27edc28e867b602ee28f6d24eaa414b605
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-07-30 14:52:15 -0300

    Change how active matches are shown in project page

--- spec/features/show_active_matches_spec.rb
+++ spec/features/show_active_matches_spec.rb
@@ -9,6 +9,7 @@ feature 'Show active matches in project page' do
            project: @project,
            user: create(:user, name: 'Neighbor.ly'),
            state: :confirmed,
+           value: 3_000,
            value_unit: 2,
            starts_at: Date.today,
            finishes_at: 3.days.from_now)
@@ -17,6 +18,7 @@ feature 'Show active matches in project page' do
            project: @project,
            user: create(:user, name: 'Foo Bar Corporation'),
            state: :confirmed,
+           value: 1_000,
            value_unit: 5,
            starts_at: Date.today,
            finishes_at: 2.days.from_now)
@@ -24,12 +26,12 @@ feature 'Show active matches in project page' do
 
   scenario 'lists the active matches' do
     visit project_path(@project)
-    expect(page).to have_text("Neighbor.ly has pledged to contribute $2 for every $1 raised between now and #{I18n.l(3.days.from_now.to_date, format: :long)}")
-    expect(page).to have_text("Foo Bar Corporation has pledged to contribute $5 for every $1 raised between now and #{I18n.l(2.days.from_now.to_date, format: :long)}")
+    expect(page).to have_text("Neighbor.ly has pledged to contribute $2 for every $1 raised up to $3,000")
+    expect(page).to have_text("Foo Bar Corporation has pledged to contribute $5 for every $1 raised up to $1,000")
   end
 
   scenario 'show summary of active matches' do
     visit project_path(@project)
-    expect(page).to have_text("Your $1 is worth $8 from now until #{I18n.l(2.days.from_now.to_date)}")
+    expect(page).to have_text("Your $1 is worth $8 from now until #{I18n.l(2.days.from_now.to_date, format: :long)}")
   end
 end
