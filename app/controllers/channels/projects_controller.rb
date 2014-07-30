e1be2dc53ca6c177fe40c77c27cebc0dd67e40ce
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-07-30 16:10:03 -0300

    Fix for bug preventing projects be associated with channels
    Reported in https://github.com/neighborly/neighborly/issues/447, the bug
    appeared given recent implementation of Strong Parameters. In the
    previous implementation, the channel id is added in request params.
    Since Channels::ProjectsController descends from ProjectsController,
    which uses the default list of parameters for any new project, not
    including the channel_id in the before_filter lambda.
    
    Also, following recent recommendations of Rails, this commit also
    changes the controller filter from "<>_filter" to "<>_action".

--- app/controllers/channels/projects_controller.rb
+++ app/controllers/channels/projects_controller.rb
@@ -1,9 +1,13 @@
 class Channels::ProjectsController < ProjectsController
   belongs_to :channel, finder: :find_by_permalink!, param: :profile_id
 
-  # Inheriting from the original Projects controller
-  # With one addition: we include the project into the current channel
-  before_filter only: [:create] { params[:project][:channels] = [@channel] }
+  after_action :associate_with_channel, only: :create
 
   prepend_before_filter{ params[:profile_id] = request.subdomain }
+
+  private
+
+  def associate_with_channel
+    resource.channels << channel
+  end
 end
