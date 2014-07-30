4dc7153f3bc566e70125d6f42d356ee60a4e5d68
Josemar Luedke <josemarluedke@gmail.com>
2014-07-30 11:24:10 -0300

    Create channel policy scope to filter which channel user can see

--- spec/policies/channel_policy_spec.rb
+++ spec/policies/channel_policy_spec.rb
@@ -33,4 +33,62 @@ describe ChannelPolicy do
       should permit(user, channel)
     end
   end
+
+  describe 'Scope' do
+    shared_examples 'being a non admin user' do
+      it 'includes online channels' do
+        _channel = create(:channel, state: :online)
+        expect(subject.resolve).to include(_channel)
+      end
+
+      it 'excludes draft channels from other users' do
+        _channel = create(:channel, state: :draft)
+        expect(subject.resolve).not_to include(_channel)
+      end
+    end
+
+    before  { create(:channel) }
+    subject { described_class::Scope.new(user, Channel.all) }
+
+    context 'for admin users' do
+      let(:user) { User.new(admin: true) }
+
+      it 'is equal to complete collection of resources' do
+        expect(subject.resolve).to eql(subject.scope)
+      end
+    end
+
+    context 'for channel owner' do
+      let(:channel) { create(:channel) }
+      let(:user)    { channel.user }
+
+      it 'includes the channel' do
+        expect(subject.resolve).to eq [channel]
+      end
+
+      it_behaves_like 'being a non admin user'
+    end
+
+    context 'for channel members' do
+      let(:channel) { create(:channel) }
+      let(:user)    { create(:user) }
+
+      before do
+        channel.members << user
+        channel.save
+      end
+
+      it 'includes the channel' do
+        expect(subject.resolve).to eq [channel]
+      end
+
+      it_behaves_like 'being a non admin user'
+    end
+
+    context 'for common users' do
+      let(:user) { create(:user) }
+
+      it_behaves_like 'being a non admin user'
+    end
+  end
 end
