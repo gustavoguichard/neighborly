88a74ef769c4287919cddf75ab6d21c9295d4a11
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-07-31 18:07:23 -0300

    Left align project boxes
    Prevents projects be alone in lines, right aligned.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,16 @@
 module ProjectsHelper
   def project_box_classes(project, is_large, contribution, columns)
     classes = if contribution && !browser.mobile?
-      'large large-12 medium-12 columns'
+      'large large-12 medium-12'
     elsif is_large
-      'large large-9 medium-8 columns'
+      'large large-9 medium-8'
     else
-      columns_n = columns || 'large-3 medium-4'
-      "#{columns_n} columns"
+      columns || 'large-3 medium-4'
     end
     classes << " #{project.category.to_s.parameterize}" if project.category
     classes << ' soon' if project.soon?
 
-    classes
+    classes << ' columns left'
   end
 
   def project_content_classes(project, is_large, contribution)
