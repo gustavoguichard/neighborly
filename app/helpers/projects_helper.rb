ee0d1fe0d27133e40e939759226ca966655d9112
Josemar Luedke <josemarluedke@gmail.com>
2014-08-07 17:02:52 -0300

    Small refactor on project show view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -20,4 +20,23 @@ module ProjectsHelper
       'large-4 medium-4 columns right'
     end
   end
+
+  def remaining_days(project)
+    [
+      content_tag(:strong, project.remaining_days),
+      project.time_to_go[:unit].capitalize,
+      pluralize_without_number(project.time_to_go[:time],
+                               t('words.remaining_singular'),
+                               t('words.remaining_plural')
+      )
+    ].join(" ").html_safe
+  end
+
+  def display_status(project)
+    content_tag :span do
+      t("projects.show.display_status.#{project.campaign_type}.#{project.display_status}",
+        goal: project.display_goal,
+        date: (l(project.expires_at.to_date, format: :long) rescue nil))
+    end
+  end
 end
