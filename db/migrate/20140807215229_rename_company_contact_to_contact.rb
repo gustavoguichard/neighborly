60e4e0824bc7242c21b5ff582723da73204df688
Josemar Luedke <josemarluedke@gmail.com>
2014-08-11 15:31:06 -0300

    Use up and down on contacts migration

--- db/migrate/20140807215229_rename_company_contact_to_contact.rb
+++ db/migrate/20140807215229_rename_company_contact_to_contact.rb
@@ -1,10 +1,15 @@
 class RenameCompanyContactToContact < ActiveRecord::Migration
-  def change
+  def up
     if ActiveRecord::Base.connection.table_exists? 'contacts'
       drop_table :contacts
     end
 
     rename_column :notifications, :company_contact_id, :contact_id
     rename_table :company_contacts, :contacts
+  end
+
+  def down
+    rename_column :notifications, :contact_id, :company_contact_id
+    rename_table :contacts, :company_contacts
   end
 end
