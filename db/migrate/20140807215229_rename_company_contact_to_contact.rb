f8d9b0959fa4b5897a46c10a6e4bc63d47e60900
Josemar Luedke <josemarluedke@gmail.com>
2014-08-11 15:29:02 -0300

    Add drop table on contacts if exists

--- db/migrate/20140807215229_rename_company_contact_to_contact.rb
+++ db/migrate/20140807215229_rename_company_contact_to_contact.rb
@@ -1,5 +1,9 @@
 class RenameCompanyContactToContact < ActiveRecord::Migration
   def change
+    if ActiveRecord::Base.connection.table_exists? 'contacts'
+      drop_table :contacts
+    end
+
     rename_column :notifications, :company_contact_id, :contact_id
     rename_table :company_contacts, :contacts
   end
