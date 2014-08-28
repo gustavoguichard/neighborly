298a607ddd1a2507b697f406bc8db7260bfb0ed7
Josemar Luedke <josemarluedke@gmail.com>
2014-08-28 11:18:31 -0300

    Remove inherited_resources from projects controller

--- app/controllers/channels/projects_controller.rb
+++ app/controllers/channels/projects_controller.rb
@@ -1,13 +1,10 @@
 class Channels::ProjectsController < ProjectsController
-  belongs_to :channel, finder: :find_by_permalink!, param: :profile_id
-
   after_action :associate_with_channel, only: :create
-
-  prepend_before_filter{ params[:profile_id] = request.subdomain }
 
   private
 
   def associate_with_channel
     resource.channels << channel
+    resource.save
   end
 end
