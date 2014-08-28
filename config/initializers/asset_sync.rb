a29bab8926d9839b30e8aca4a1d63fd8f8037b9b
Josemar Luedke <josemarluedke@gmail.com>
2014-08-28 10:34:12 -0300

    Revert "Let's delete existing files from assets pipeline"
    This reverts commit a42a8ee4c11c54621e4c8f409a99794f33828d97.

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -14,7 +14,7 @@ AssetSync.configure do |config|
   # config.fog_region = Configuration[:aws_region]
 
   # Don't delete files from the store
-   config.existing_remote_files = "delete"
+  # config.existing_remote_files = "keep"
   #
   # Automatically replace files with their equivalent gzip compressed version
   # config.gzip_compression = true
