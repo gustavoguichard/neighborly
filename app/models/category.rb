98f0963aaa83c5bc8f40899c72a4e593d06bdcf7
Josemar Luedke <josemarluedke@gmail.com>
2014-09-10 15:08:21 -0700

    Add name to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,10 @@ class Category < ActiveRecord::Base
   validates_presence_of :name_pt
   validates_uniqueness_of :name_pt
 
+  def name
+    to_s
+  end
+
   def self.with_projects
     where("exists(select true from projects p where p.category_id = categories.id and p.state not in('draft', 'rejected'))")
   end
