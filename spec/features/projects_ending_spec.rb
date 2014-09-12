3dc8bc2f59de9fe7ccf2cb496407fea446725dd2
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-09-11 21:26:52 -0300

    Ensure contributions are disabled after projects ending

---
+++ spec/features/projects_ending_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Project\'s ending' do
+  include ActiveSupport::Testing::TimeHelpers
+
+  background do
+    @project = create(:project, state: 'online', online_date: -1.seconds.from_now)
+    @user = create(:user, password: 'test123')
+    login
+  end
+
+  scenario 'After reaching ending date' do
+    @project.finish
+    travel (@project.online_days + 1).days
+
+    visit '/'
+    click_on 'Discover'
+    click_on @project.name
+    expect(page).to_not have_link('Contribute')
+  end
+end
