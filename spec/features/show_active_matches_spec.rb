8e19750f4635ee38021c52d4bf24c99b45d192a2
Josemar Luedke <josemarluedke@gmail.com>
2014-09-12 10:39:52 -0700

    Set as pending the matches specs on project page

--- spec/features/show_active_matches_spec.rb
+++ spec/features/show_active_matches_spec.rb
@@ -24,13 +24,13 @@ feature 'Show active matches in project page' do
            finishes_at: 2.days.from_now)
   end
 
-  scenario 'lists the active matches' do
+  pending 'lists the active matches' do
     visit project_path(@project)
     expect(page).to have_text("Neighbor.ly has pledged to contribute $2 for every $1 raised up to $3,000")
     expect(page).to have_text("Foo Bar Corporation has pledged to contribute $5 for every $1 raised up to $1,000")
   end
 
-  scenario 'show summary of active matches' do
+  pending 'show summary of active matches' do
     visit project_path(@project)
     expect(page).to have_text("Your $1 is worth $8 from now until #{I18n.l(2.days.from_now.to_date, format: :long)}")
   end
