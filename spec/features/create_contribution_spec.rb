69cc4c27a45e8fda96e3ed451ed10dabac228bad
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-09-19 15:18:17 -0300

    Update contribution feature spec with the new view

--- spec/features/create_contribution_spec.rb
+++ spec/features/create_contribution_spec.rb
@@ -2,27 +2,16 @@ require 'spec_helper'
 
 feature 'Create Investment' do
   background do
-    @project = create(:project, state: 'online', sale_date: -1.seconds.from_now)
+    @project = create(:project, :with_rewards)
     @user = create(:user, password: 'test123')
     login
   end
 
-  scenario 'providing value smaller then accepted' do
+  scenario 'providing order size' do
     visit project_path(@project)
     click_on 'Invest'
 
-    fill_in 'contribution_form_value', with: 9
-
-    expect {
-      click_on 'Review & Proceed'
-    }.to_not change(Contribution, :count)
-  end
-
-  scenario 'providing value accepted' do
-    visit project_path(@project)
-    click_on 'Invest'
-
-    fill_in 'contribution_form_value', with: 10
+    find(:xpath, "//input[@id='contribution_form_value']").set '10'
 
     expect {
       click_on 'Review & Proceed'
