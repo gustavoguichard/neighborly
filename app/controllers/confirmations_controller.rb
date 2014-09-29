e3425e515588d1e6c89dc9aa478ef0c1c0d60105
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-09-29 18:12:48 -0300

    Refactor confirmations controller to remove duplication

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,17 +1,18 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def show
     confirmation_token = Devise.token_generator.
-      digest(self, :confirmation_token, token)
-    self.resource = resource_class.find_by(confirmation_token: confirmation_token)
+      digest(self, :confirmation_token, token_param)
+    self.resource = resource_class.
+      find_by(confirmation_token: confirmation_token)
 
-    super if resource.nil? or resource.confirmed?
+    super unless resource
   end
 
   def confirm
-    self.resource = resource_class.find_by(confirmation_token: token)
+    self.resource = resource_class.find_by(confirmation_token: token_param)
     resource.assign_attributes(permitted_params)
 
-    if resource.valid? && resource.password_match?
+    if resource.valid?
       resource.confirm!
       set_flash_message :notice, :confirmed
       sign_in_and_redirect resource_name, resource
@@ -26,7 +27,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     params.require(resource_name).permit(:confirmation_token, :password, :password_confirmation)
   end
 
-  def token
+  def token_param
     params[:confirmation_token] ||
       params[resource_name].try(:[], :confirmation_token)
   end
