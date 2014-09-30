6694ce3e20b050fcbd86e2288d576c66651d72d4
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-09-30 15:29:19 -0300

    Yes, I am sure I want to not sign up for any newsletter

--- app/workers/welcome_worker.rb
+++ app/workers/welcome_worker.rb
@@ -6,6 +6,5 @@ class WelcomeWorker
     user = User.find(user_id)
 
     Notification.notify_once(:new_user_registration, user, user_id: user.id)
-    user.update_attribute(:newsletter, true)
   end
 end
