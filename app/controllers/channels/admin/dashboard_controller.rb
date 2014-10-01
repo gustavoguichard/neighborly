ca4389d446fd0016950f50958e7eed8ff81ef71b
Josemar Luedke <josemarluedke@gmail.com>
2014-10-01 14:20:40 -0700

    Remove channels admin

--- app/controllers/channels/admin/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-module Channels::Admin
-  class DashboardController < BaseController
-    def index
-      @total_projects = channel.projects.size
-      @total_subscribers = channel.subscribers.count
-    end
-  end
-end
