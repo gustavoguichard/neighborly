be190e024c4831c584596f6c10849274f21a8f4e
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-10-01 11:36:03 -0300

    Generate referral code to new users

--- db/migrate/20140930202758_add_referrer_fields_to_users.rb
+++ db/migrate/20140930202758_add_referrer_fields_to_users.rb
@@ -1,6 +1,18 @@
+class User < ActiveRecord::Base; end
+
 class AddReferrerFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :referrer_id, :integer, foreign_key: { references: :users }
     add_column :users, :referral_code, :string
+
+    reversible do |dir|
+      dir.up do
+        users = User.where(referral_code: nil) +
+          User.where(referral_code: '')
+        users.each do |user|
+          user.update_attributes(referral_code: SecureRandom.urlsafe_base64)
+        end
+      end
+    end
   end
 end
