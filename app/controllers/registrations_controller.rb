54955d1bfaccd80c76d494c7ebe4142ddb22001f
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-10-01 21:10:43 -0300

    Use referral_code directly from session accessor

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-  def new
-    @referral_code = referral_code
-
-    super
-  end
-
   def create
     if referral_code.present?
       referrer = User.find_by(referral_code: referral_code)
