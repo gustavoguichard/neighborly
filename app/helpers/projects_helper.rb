d6334c1d71e28128b065645478cd39522fd9bce1
Josemar Luedke <josemarluedke@gmail.com>
2014-10-02 10:46:20 -0700

    Remove contribution info from project box

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,8 +1,6 @@
 module ProjectsHelper
-  def project_box_classes(project, is_large, contribution, columns)
-    classes = if contribution && !browser.mobile?
-      'large large-12 medium-12'
-    elsif is_large
+  def project_box_classes(project, is_large, columns)
+    classes = if is_large
       'large large-9 medium-8'
     else
       columns || 'large-3 medium-4'
@@ -10,14 +8,6 @@ module ProjectsHelper
     classes << ' soon' if project.soon?
 
     classes << ' columns left'
-  end
-
-  def project_content_classes(project, is_large, contribution)
-    if contribution && !browser.mobile?
-      'large-3 medium-3 columns right'
-    elsif is_large
-      'large-4 medium-4 columns right'
-    end
   end
 
   def remaining_days(project)
