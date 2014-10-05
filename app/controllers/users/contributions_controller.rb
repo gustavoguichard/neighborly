e0d6918613c9f3bbb3bbbfb1d9c166ae71298509
Josemar Luedke <josemarluedke@gmail.com>
2014-10-04 20:52:54 -0700

    Remove inherit_resources from user contributions controller

--- app/controllers/users/contributions_controller.rb
+++ app/controllers/users/contributions_controller.rb
@@ -1,26 +1,22 @@
 class Users::ContributionsController < ApplicationController
   after_filter :verify_authorized, except: :index
   after_filter :verify_policy_scoped, only: :index
-  inherit_resources
-  defaults resource_class: Contribution
-  belongs_to :user
-  actions :index
 
   def index
     authorize parent, :update?
-    index!
+    @contributions = policy_scope(parent.contributions).
+      order("created_at DESC").
+      includes(:reward, :project)
   end
 
-  protected
+  private
+
   def policy_scope(scope)
     @_policy_scoped = true
     ContributionPolicy::UserScope.new(current_user, parent, scope).resolve
   end
 
-  def collection
-    @contributions ||= policy_scope(end_of_association_chain).
-      order("created_at DESC, confirmed_at DESC").
-      includes(:user, :reward, project: [:user]).
-      page(params[:page]).per(10)
+  def parent
+    @user ||= User.find(params[:user_id])
   end
 end
