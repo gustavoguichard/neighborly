3daa7e4a086e80523aa2ab9b99b2ae9bdbf1e5c6
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-10-13 18:30:41 -0300

    Revert "Remove outdated comment and related code"
    This reverts commit 2aba0d62b480657bfbf2571f23b2c309d3e20566.

--- app/policies/project_inherited_policy_helpers.rb
+++ app/policies/project_inherited_policy_helpers.rb
@@ -6,4 +6,9 @@ module ProjectInheritedPolicyHelpers
   def destroy?
     create?
   end
+
+  # This method is used in ApplicationPolicy#done_by_owner_or_admin?
+  def is_owned_by?(user)
+    user.present? && record.project.user == user
+  end
 end
