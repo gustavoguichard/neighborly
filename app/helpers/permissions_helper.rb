9441e09a6c429c2de366478bc973bca5aab4f781
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-10-14 22:08:21 -0300

    Make links to project_path compatible with permissions module

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -1,5 +1,9 @@
 module PermissionsHelper
   def permissions_discover_attrs
-    { 'data-hyperlink-permission' => policy(Project).discover?, 'data-name' => 'Discover issuances' }
+    { 'data-hyperlink-permission' => policy(Project).discover?, 'data-name' => 'Wants to discover issuances' }
+  end
+
+  def permissions_project_attrs(project = @project)
+    { 'data-hyperlink-permission' => policy(project).show?, 'data-name' => 'Wants to view issuance' }
   end
 end
