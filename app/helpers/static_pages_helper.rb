52c16f69e95ede1d9c6eacafcaf594bee61682d3
Josemar Luedke <josemarluedke@gmail.com>
2014-10-19 21:47:28 -0700

    Remove entypo font

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,9 +1,9 @@
 module StaticPagesHelper
   def pricing_table_item(name, is_available)
     title, klass = if is_available
-      ['Item included', %w(included icon-et-check)]
+      ['Item included', %w(included fa-check)]
     else
-      ['Item not included', %w(not-included icon-fa-remove)]
+      ['Item not included', %w(not-included fa-remove)]
     end
 
     content_tag :li, class: 'bullet-item' do
